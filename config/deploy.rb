a2f17deda06530d3501df4badcb15bc1871e48da
Michael Chittenden <mchitten@gmail.com>
2013-11-05 15:59:21 -0500

    Deploy to qa was successful.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,14 @@ end
 
 set :application, "DoSomething.org"
 set :repository,  "git@github.com:DoSomething/dosomething.git"
-set :branch, 'master'
+set :branch, 'dev'
+
+if @config['default'] && (@config['default']['scm_user'] && @config['default']['scm_pass'])
+  set :scm, :git
+  set :scm_user, @config['default']['scm_user']
+  set :scm_passphrase, @config['default']['scm_pass']
+end
+
 set :stages, %w{ production qa staging }
 set :default_stage, "production"
 
@@ -19,7 +26,6 @@ if @config['default']
   set :password, @config['default']['password']
 end
 
-#ssh_options[:keys] = [ENV['CAP_PRIVATE_KEY']]
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
@@ -29,9 +35,5 @@ set :use_sudo, false
 namespace :deploy do
   task :start do; end
   task :stop do; end
-  task :go, stage: "qa" do
-    run "cd /var/www && ls -l"
-    abort "Testing."
-  end
 end
 
