2cc4d0889888a0682a920157f12b274eb2e8be73
Michael Chittenden <mchitten@gmail.com>
2013-11-08 11:07:46 -0500

    Documentation.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
+# Basic configuration for the production environment.  Please see
+# https://github.com/DoSomething/dosomething/wiki/Setting-up-deploy-vars
+# for more details.
+
 role :app, *@config['production']['servers'], primary: true
 set :gateway, @config['production']['gateway']
 set :port, @config['production']['port']
