a463b2ef24d8bfe425e4e378e87baca102739c4c
Michael Chittenden <mchitten@gmail.com>
2013-11-12 10:39:17 -0500

    This file works.

---
+++ works.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$abc = FALSE;
+if ($abc == FALSE) {
+  echo ":(";
+}
+
+/**
+ * Tests something or other.
+ *
+ * @param string $assertion
+ *   A string of assertions.
+ *
+ * @return string
+ *   dogs or cats
+ */
+function test($assertion) {
+  if ($assertion == "cat") {
+    return "banana";
+  }
+
+  return "dogs";
+}
