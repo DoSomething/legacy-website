4c552eaa583f69ce72d5914049b2d689f320f969
Aaron Schachter <aaronschachter@gmail.com>
2013-12-13 21:52:17 +0000

    Output action_type and cause terms

--- themes/dosomething/paraneue_dosomething/templates/campaign/campaign.tpl.php
+++ themes/dosomething/paraneue_dosomething/templates/campaign/campaign.tpl.php
@@ -4,11 +4,31 @@
 		<p><?php print $campaign->stat_problem; ?></p>
 		<p><?php print $campaign->stat_solution; ?></p>
 	</div>
-	<?php if (!empty($gallery)): ?>
+
+	<?php if (!empty($campaign->action_type)): ?>
+  <h4>Action Type</h4>
+  <ul>
+  <?php foreach ($campaign->action_type as $term): ?>
+    <li><?php print l($term['name'], 'taxonomy/term/' . $term['tid']); ?></li>
+  <?php endforeach; ?>
+  </ul>
+  <?php endif; ?>
+
+	<?php if (!empty($campaign->cause)): ?>
+  <h4>Cause</h4>
+  <ul>
+  <?php foreach ($campaign->cause as $term): ?>
+    <li><?php print l($term['name'], 'taxonomy/term/' . $term['tid']); ?></li>
+  <?php endforeach; ?>
+  </ul>
+  <?php endif; ?>
+
+	<?php if (!empty($campaign->gallery)): ?>
   <div id="gallery">
-  <?php foreach ($gallery as $gallery_image): ?>
-    <img src="<?php print $gallery_image['src']; ?>" alt="<?php print $gallery_image['alt']; ?>" />
+  <?php foreach ($campaign->gallery as $image): ?>
+    <img src="<?php print $image['src']; ?>" alt="<?php print $image['alt']; ?>" />
   <?php endforeach; ?>
   </div>
   <?php endif; ?>
+
 </div>
