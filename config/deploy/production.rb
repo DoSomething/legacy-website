42a4e203d6826b609b900e7ef53374b1a938f31e
Desmond Morris <hi@desmondmorris.com>
2014-01-09 10:51:19 -0500

    Removes capistrano deploy configuration files and other related files

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-# Basic configuration for the production environment.  Please see
-# https://github.com/DoSomething/dosomething/wiki/Deployments
-# for more details.
-
-role :app, *@config['production']['servers'], primary: true
-set :gateway, @config['production']['gateway']
-set :port, @config['production']['port']
-set :branch, @config['production']['branch']
-set :deploy_to, @config['production']['deploy_to']
