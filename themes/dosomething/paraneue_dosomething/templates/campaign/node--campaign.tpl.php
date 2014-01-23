3f075ac84d42161ae4e75d3bc2d26974ad716d55
Aaron Schachter <aaronschachter@gmail.com>
2014-01-23 10:54:32 -0500

    Fixes #442

--- themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
+++ themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
@@ -29,11 +29,13 @@
   <?php endforeach; ?>
   <?php endif; ?>
 
+  <?php if (isset($faq)): ?>
   <h3>FAQ</h3>
   <?php foreach ($faq as $item): ?>
   <h4><?php print $item['header']; ?></h4>
   <p><?php print $item['copy'] ?></p>
   <?php endforeach; ?>
+  <?php endif; ?>
 
   <h2> Plan It </h2>
   <?php if (isset($starter)) : ?>
@@ -56,17 +58,21 @@
   <h4> <?php print $pre_step_header; ?> </h4>
   <?php print $pre_step_copy['safe_value']; ?>
 
+  <?php if (isset($step_pre)) : ?>
   <h3>Pre Steps</h3>
   <?php foreach ($step_pre as $item): ?>
   <h4><?php print $item['header']; ?></h4>
   <p><?php print $item['copy'] ?></p>
   <?php endforeach; ?>
+  <?php endif; ?>
 
+  <?php if (isset($step_post)) : ?>
   <h3>Post Steps</h3>
   <?php foreach ($step_post as $item): ?>
   <h4><?php print $item['header']; ?></h4>
   <p><?php print $item['copy'] ?></p>
   <?php endforeach; ?>
+  <?php endif; ?>
 
   <h4> Snap a Pic </h4>
   <?php print $pic_step['safe_value']; ?>
