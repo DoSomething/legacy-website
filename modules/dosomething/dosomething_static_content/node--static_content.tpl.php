8a0135716a2d6d9d5d922f5cff075d4165038f08
Morgan Rich <mrich@dosomething.org>
2014-01-24 22:14:23 +0000

    Adding themed images and some markup
    * Using image functions to add themed images to template
    * Printing out available variables in the template

--- modules/dosomething/dosomething_static_content/node--static_content.tpl.php
+++ modules/dosomething/dosomething_static_content/node--static_content.tpl.php
@@ -1,27 +1,37 @@
-<h1>THIS IS THE MODULE TEMPLATE</h1>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      //print render($content);
-    ?>
-  </div>
 
   <div>
     <h1><?php print $title; ?></h1>
+    <?php print $hero_image; ?>
     <?php if ($subtitle): ?>
       <h2><?php print $subtitle; ?></h2>
     <?php endif; ?>
   </div>
+  <h2><?php print $intro_title; ?></h2>
+  <div class="left"><?php print $intro; ?></div>
+  <?php print $intro_image; ?>
+  <div class="cta">
+    <h2><?php print $cta; ?></h2>
+    <div class="cta_button btn"><?php print $cta_button; ?></div>
+  </div>
+ <br/>
+  <?php foreach ($galleries as $gallery): ?>
+    <?php print $gallery['image']; ?>
+    <h3><?php print $gallery['image_title']; ?></h3>
+    <p><?php print $gallery['image_description']; ?></p>
+  <?php endforeach; ?>
 
+  
+  <h2><?php print $additional_text_title; ?></h2>
+  <p><?php print $additional_text; ?></p>
 
+  <div class="cta">
+    <h2><?php print $cta; ?></h2>
+    <div class="cta_button btn"><?php print $cta_button; ?></div>
+  </div>
 
 </article>
