573a66eaae444ada8cad3b899757de07234ba909
Aaron Schachter <aaronschachter@gmail.com>
2014-01-25 11:07:35 -0500

    Output new properties to reportback view

--- themes/dosomething/paraneue_dosomething/templates/reportback/reportback.tpl.php
+++ themes/dosomething/paraneue_dosomething/templates/reportback/reportback.tpl.php
@@ -16,4 +16,10 @@
 </nav>
 
 <p><strong><?php print render($content['quantity_count']); ?></strong> <?php print render($content['quantity_label']); ?></p>
+<p><?php print render($content['why_participated']); ?></p>
+
+<?php if (isset($content['num_participants'])): ?> 
+<p><?php print render($content['num_participants']); ?> participants</p>
+<?php endif; ?>
+
 <?php print render($content); ?>
