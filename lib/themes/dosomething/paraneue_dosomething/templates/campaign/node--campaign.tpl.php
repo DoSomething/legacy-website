558650c0d87ef36ea75c008e0bfed297db36a4a3
mmwtsn <mwatson@dosomething.org>
2014-02-06 15:39:55 -0500

    Improves semantics and sources hero image from node

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
@@ -1,16 +1,26 @@
-<div class="content clearfix <?php print $classes; ?>">
-<?php if (isset($hero_image_l)) : ?>
-  <?php print $hero_image_l; ?>
-<?php endif; ?>
-<?php print $cta; ?>
-<div class="sponsor">
-  <?php if (isset($sponsors)): ?>
-    <?php foreach ($sponsors as $key => $sponsor) :?>
-      <?php print $sponsor['name']; ?>
-      <?php // print $sponsor['img']; ?>
-    <?php endforeach; ?>
-  <?php endif; ?>
-</div>
+<section class="campaign-wrapper <?php print $classes; ?>" style="background-image: url(<?php print $hero_image_l_url; ?>);">
+  <header class="campaign-meta">
+    <h1 class="campaign-title"><?php print $title; ?></h1>
+
+    <p class="campaign-cta"><?php print $cta; ?></p>
+
+    <?php print render($signup_form); ?>
+
+    <img class="campaign-arrow" src="https://trello-attachments.s3.amazonaws.com/52de9089aa3032b85e9b0962/52e1724e23eeb26f4e9fc427/7e9e3ef8974d815230449b9829e98ac0/arrow.png" alt="Click the button!" />
+    <p class="campaign-scholarship"><span class="highlight"><?php print $scholarship; ?></span></p>
+
+    <?php print $cta; ?>
+
+    <div class="sponsor">
+      <?php if (isset($sponsors)): ?>
+        <?php foreach ($sponsors as $key => $sponsor) :?>
+          <?php print $sponsor['name']; ?>
+          <?php // print $sponsor['img']; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </div>
+  </header>
+
 
   <h2> Know It </h2>
   <?php if (isset($fact_problem)): ?>
@@ -112,5 +122,4 @@
   <a class="btn large"><?php print $reportback_link_label; ?></a>
 
   <?php print render($reportback_form); ?>
-
-</div>
+</section>
