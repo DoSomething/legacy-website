41fa8e2d1ad23636342fb6d2e4e6e0bab96d6305
Andrea Gaither <angaither@gmail.com>
2014-02-07 12:23:44 -0500

    Update signup button var name in pitch tpl.

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
@@ -2,9 +2,7 @@
   <div class="campaign-container">
     <h1 class="campaign-title"><?php print $title; ?></h1>
     <p class="campaign-cta"><?php print $cta; ?></p>
-
-    <?php print render($signup_form); ?>
-
+      <?php print render($signup_button); ?>
     <img class="campaign-arrow" src="https://trello-attachments.s3.amazonaws.com/52de9089aa3032b85e9b0962/52e1724e23eeb26f4e9fc427/7e9e3ef8974d815230449b9829e98ac0/arrow.png" alt="Click the button!" />
     <p class="campaign-scholarship"><span class="highlight"><?php print $scholarship; ?></span></p>
   </div>
