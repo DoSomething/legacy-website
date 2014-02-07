0543b4465b5868fcdbc09809c748ddeae1352fbf
mmwtsn <mwatson@dosomething.org>
2014-02-07 12:50:36 -0500

    Namespacing! Re-strucutres the templates with new wrappers

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
@@ -1,11 +1,20 @@
-<section class="campaign-wrapper <?php print $classes; ?>" style="background-image: url(<?php print $hero_image_l_url; ?>);">
-  <header class="campaign-meta">
-    <h1 class="campaign-title"><?php print $title; ?></h1>
-    <p class="campaign-cta"><?php print $cta; ?></p>
+<section class="c--wrapper">
+  <header class="header <?php print $classes; ?>" style="background-image: url(<?php print $hero_image_l_url; ?>);">
+    <h1 class="title"><?php print $title; ?></h1>
+    <p class="cta"><?php print $cta; ?></p>
 
     <?php print render($signup_form); ?>
 
-    <img class="campaign-arrow" src="https://trello-attachments.s3.amazonaws.com/52de9089aa3032b85e9b0962/52e1724e23eeb26f4e9fc427/7e9e3ef8974d815230449b9829e98ac0/arrow.png" alt="Click the button!" />
-    <p class="campaign-scholarship"><span class="highlight"><?php print $scholarship; ?></span></p>
+    <img class="arrow" src="https://trello-attachments.s3.amazonaws.com/52de9089aa3032b85e9b0962/52e1724e23eeb26f4e9fc427/7e9e3ef8974d815230449b9829e98ac0/arrow.png" alt="Click the button!" />
+    <p class="scholarship"><span class="highlight"><?php print $scholarship; ?></span></p>
+
+    <div class="sponsor">
+      <?php if (isset($sponsors)): ?>
+        <?php foreach ($sponsors as $key => $sponsor) :?>
+          <?php print $sponsor['name']; ?>
+          <?php // print $sponsor['img']; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </div>
   </header>
 </section>
