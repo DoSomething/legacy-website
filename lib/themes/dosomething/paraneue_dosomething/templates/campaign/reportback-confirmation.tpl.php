ca1b8f794554fef35f53102eaf9d40b7c84c7415
Aaron Schachter <aaronschachter@gmail.com>
2014-02-20 11:23:46 -0500

    Adds recommended campaign vars into confirmation tpl

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/reportback-confirmation.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/reportback-confirmation.tpl.php
@@ -3,5 +3,17 @@
 <p><?php print $copy; ?></p>
 
 <?php print $more_campaigns_link; ?>
-<hr>
+
 <?php print $back_to_campaign_link; ?>
+
+<div class="row">
+<?php foreach ($recommended as $rec): ?>
+  <div class="column span_4">
+    <?php if (isset($rec['image'])): ?>
+      <?php print $rec['image']; ?>
+    <?php endif; ?>
+    <h3><?php print $rec['title']; ?></h3>
+    <p><?php print $rec['call_to_action']; ?></p>
+  </div>
+<?php endforeach; ?>
+</div>
