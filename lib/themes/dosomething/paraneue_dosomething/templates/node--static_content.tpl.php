dc3bcd0bc43cbebc88f9da1e37c02f49ecc7a891
Barry Clark <barry@barryclark.co>
2014-02-25 17:17:26 -0500

    First pass over tpl file markup.

--- lib/themes/dosomething/paraneue_dosomething/templates/node--static_content.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/node--static_content.tpl.php
@@ -1,52 +1,58 @@
 <section class="static_content--wrapper">
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-    <?php if (!$page): ?>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
+    <header class="header">
+      <h1 class="title"><?php print $title; ?></h1>
+      <?php if (isset($subtitle)): ?>
+	    <p class="subtitle"><?php print $subtitle; ?></p>
+      <?php endif; ?>
+    </header>
 
-    <div>
-      <h1><?php print $title; ?></h1>
-      <?php if (isset($hero_image)): ?>
-        <?php print $hero_image; ?>
-      <?php endif; ?>
-      <?php if ($subtitle): ?>
-        <h2><?php print $subtitle; ?></h2>
-      <?php endif; ?>
+    <div class="intro">
+	 <?php if (isset($intro_title)): ?>
+	  <h2><?php print $intro_title; ?></h2>
+	<?php endif; ?>
+	<?php if (isset($intro)): ?>
+	  <div class="intro-content"><?php print $intro; ?></div>
+	<?php endif; ?>
+	<?php if (isset($intro_image)): ?>
+	  <?php print $intro_image; ?>
+	<?php elseif (isset($intro_video)): ?>
+	  <?php print $intro_video; ?>
+	<?php endif; ?>
     </div>
-    <h2><?php print $intro_title; ?></h2>
-    <div class="left"><?php print $intro; ?></div>
-    <?php if (isset($intro_image)): ?>
-      <?php print $intro_image; ?>
-    <?php elseif (isset($intro_video)): ?>
-      <?php print $intro_video; ?>
-    <?php endif; ?>
+
     <?php if (isset($call_to_action)): ?>
       <div class="cta">
         <h2><?php print $call_to_action; ?></h2>
         <div class="cta_button"><?php print $cta_link; ?></div>
       </div>
     <?php endif; ?>
-   <br/>
-   <?php if (isset($galleries)): ?>
-      <?php foreach ($galleries as $gallery): ?>
-        <?php print $gallery['image']; ?>
-        <?php if ($gallery['image_title']): ?>
-          <h3><?php print $gallery['image_title']; ?></h3>
-        <?php endif; ?>
-        <?php if ($gallery['image_description']): ?>
-          <p><?php print $gallery['image_description']; ?></p>
-        <?php endif; ?>
-      <?php endforeach; ?>
+
+    <?php if (isset($galleries)): ?>
+      <div class="gallery">
+	<?php foreach ($galleries as $gallery): ?>
+	  <?php print $gallery['image']; ?>
+	  <?php if ($gallery['image_title']): ?>
+	    <h3><?php print $gallery['image_title']; ?></h3>
+	  <?php endif; ?>
+	  <?php if ($gallery['image_description']): ?>
+	    <p><?php print $gallery['image_description']; ?></p>
+	  <?php endif; ?>
+	<?php endforeach; ?>
+      </div>
     <? endif; ?>
-    
-    <?php if (isset($additional_text_title)): ?>
-      <h2><?php print $additional_text_title; ?></h2>
-    <?php endif; ?>
+
     <?php if (isset($additional_text)): ?>
+    <div class="additional-text">
+      <?php if (isset($additional_text_title)): ?>
+	<h2><?php print $additional_text_title; ?></h2>
+      <?php endif; ?>
+
       <p><?php print $additional_text; ?></p>
+    </div>
     <?php endif; ?>
-    
+
     <?php if (isset($call_to_action)): ?>
       <div class="cta">
         <h2><?php print $call_to_action; ?></h2>
