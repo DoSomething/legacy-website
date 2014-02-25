4b6b84174b5dba0d5fc415a1fa37c1627ae77e07
David Furnes <dfurnes@dosomething.org>
2014-02-24 20:12:42 -0500

    Adds html5shiv for IE8.

---
+++ lib/themes/dosomething/paraneue_dosomething/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements hook_js_alter().
+ */
+function paraneue_dosomething_js_alter(&$js) {
+  $html5shiv = NEUE_PATH . '/bower_components/dist/html5shiv.js';
+  $js['html5shiv'] = drupal_js_defaults();
+  $js['html5shiv']['data'] = $html5shiv;
+  $js['html5shiv']['scope'] = 'header';
+  $js['html5shiv']['group'] = -100;
+  $js['html5shiv']['browsers'] = array('IE' => 'lte IE 8');
+  $js['html5shiv']['type'] = 'file';
+  $js['html5shiv']['every_page'] = TRUE;
+  $js['html5shiv']['weight'] = -50;
+  dpm($js);
+}
+
+?>
