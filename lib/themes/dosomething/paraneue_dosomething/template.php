831ff40c7bb5d40ad16c47fcce3c8f2aa9648f9b
David Furnes <dfurnes@dosomething.org>
2014-02-28 01:46:47 -0500

    Split template.php into partials for organization.

--- lib/themes/dosomething/paraneue_dosomething/template.php
+++ lib/themes/dosomething/paraneue_dosomething/template.php
@@ -4,12 +4,18 @@
  * Implements hook_js_alter().
  */
 function paraneue_dosomething_preprocess_page(&$vars, $hook) {
-  $html5shiv = array(
-    '#type' => 'markup',
-    '#markup' => '<!--[if lte IE 8]> <script type="text/javascript" src="' . NEUE_PATH . '/bower_components/dist/html5shiv.js' . '"></script> <![endif]-->'
-  );
+  include('template_hooks/preprocess_page/html5shiv.php');
 
-  drupal_add_html_head($html5shiv, 'html5shiv');
 }
+
+/**
+ * Implements hook_form_alter();
+ */
+function paraneue_dosomething_form_alter(&$form, &$form_state, $form_id) {
+}
+
+/**
+ * Custom '#after_build' functions on form elements.
+ */
 
 ?>
