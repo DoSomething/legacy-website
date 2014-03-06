032f12e38e59fe684d7dabdeece666d2cd16e3d6
Barry Clark <barry@barryclark.co>
2014-03-06 14:55:06 -0500

    Naming update: FACT not FACTS

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/node--fact_page.tpl.php
@@ -0,0 +1,54 @@
+<section class="fact_page-wrapper">
+  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+    <div class="header-wrapper">
+      <header class="header">
+        <h1 class="title"><?php print $title; ?></h1>
+        <?php if (isset($subtitle)): ?>
+        <h3 class="subtitle"><?php print $subtitle; ?></h3>
+        <?php endif; ?>
+      </header>
+    </div>
+
+    <div class="intro-wrapper">
+      <div class="intro">
+        <?php if (isset($intro_title)): ?>
+          <h2><?php print $intro_title; ?></h2>
+        <?php endif; ?>
+        <?php if (isset($intro)): ?>
+          <div class="intro-content"><?php print $intro; ?></div>
+        <?php endif; ?>
+        <?php if (isset($intro_image)): ?>
+          <?php print $intro_image; ?>
+        <?php elseif (isset($intro_video)): ?>
+          <?php print $intro_video; ?>
+        <?php endif; ?>
+      </div>
+    </div>
+
+    <?php if (isset($facts)): ?>
+    <?php foreach ($facts as $key => $fact): ?>
+      <p>
+        <?php print ($key + 1) . '. ' . $fact['fact']; ?>
+        <sup><?php print $fact['footnotes']; ?></sup>
+      </p>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
+  <?php if (isset($sources)): ?>
+    <h4>Sources</h4>
+    <?php foreach ($sources as $key => $source): ?>
+      <p><sup><?php print ($key + 1); ?></sup> <?php print $source; ?></p>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
+    <?php if (isset($call_to_action)): ?>
+      <div class="cta-wrapper">
+        <div class="cta">
+          <h3><?php print $call_to_action; ?></h3>
+          <div class="cta_button"><?php print $cta_link; ?></div>
+        </div>
+      </div>
+    <?php endif; ?>
+  </article>
+</section>
