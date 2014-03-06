f80c22e44db495db8eceab13c82b8aaf2aa9af6c
Jon Uy <jonxuy@gmail.com>
2014-03-06 11:25:08 -0500

    Function name changes from mobile_commons to mobilecommons

--- lib/modules/dosomething/dosomething_sms/plugins/activity/gateways/start_campaign/ConductorActivityStartCampaignGateway.class.php
+++ lib/modules/dosomething/dosomething_sms/plugins/activity/gateways/start_campaign/ConductorActivityStartCampaignGateway.class.php
@@ -24,10 +24,10 @@ class ConductorActivityStartCampaignGateway extends ConductorActivity {
       $route = $this->routes[$mdataID];
 
       // Opt the user out of the transactional campaign
-      dosomething_sms_mobile_commons_opt_out($mobile, $route['out_campaign_id']);
+      dosomething_sms_mobilecommons_opt_out($mobile, $route['out_campaign_id']);
 
       // Opt the user into a path in the actual campaign
-      dosomething_sms_mobile_commons_opt_in($mobile, $route['opt_in_path_id']);
+      dosomething_sms_mobilecommons_opt_in($mobile, $route['opt_in_path_id']);
     }
 
     $state->setContext('ignore_no_response_error', TRUE);
