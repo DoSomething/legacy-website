c8d3708f42bfa6430920acbb2a75219dcb636632
Andrea Gaither <angaither@gmail.com>
2014-03-06 16:43:55 -0500

    Added mobile data to user object from old world.

--- scripts/import-users.php
+++ scripts/import-users.php
@@ -3,32 +3,53 @@
  * Script to import users from old world into new world.
  *
  * to run:
- * import user data from old world site into temporary table
+ * import user data from old world site into temporary tables
+ * will need 'users', 'profile', and 'field_data_field_user_mobile'.
  * drush --script-path=../scripts/ php-script import-users.php
  *
  */
 
 $user_table = '_users_old_world';
+$profile_id = '_profile_old_world';
+$mobile_data = '_mobile_old_world';
 
-//@TODO: don't limit this.
-$result = db_query('SELECT * FROM {' . $user_table . '} LIMIT 5');
+$result = db_query('SELECT * FROM {' . $user_table . '}');
 
-foreach ($result as $row) {
+foreach ($result as $user_row) {
   // Only migrate users with active status.
-  if ($row->status == 1) {
+  if ($user_row->status == 1) {
     $user = array();
+    // Will be overridden by uid.
     $user['name'] = user_password(8);
-    $user['pass'] = $row->pass;
-    $user['mail'] = $row->mail;
-    $user['created'] = $row->created;
-    $user['access'] = $row->access;
+    $user['pass'] = $user_row->pass;
+    $user['mail'] = $user_row->mail;
+    $user['created'] = $user_row->created;
+    $user['access'] = $user_row->access;
+    // Set the init value as the old world uid.
+    $user['init']= $user_row->uid;
     $account = user_save('', $user);
-
-    // Keep uids.
-    $old_uid = $row->uid;
-    $new_uid = $account->uid;
   }
+
+  // Some users only have mobile as login method.
+  $mobile_result = db_query("SELECT  m.field_user_mobile_value
+                             FROM {$mobile_data} m
+                             INNER JOIN {$profile_id} p on m.entity_id = p.pid
+                             WHERE p.uid = $user_row->uid")->fetchField();
+  if ($mobile_result) {
+    $edit = array(
+      'field_mobile' => array(
+        LANGUAGE_NONE => array(
+          0 => array(
+            'value' => $mobile_result,
+          ),
+        ),
+      ),
+    );
+    // Save account with mobile
+    user_save($account, $edit);
+  }
+
 }
 
-//@TODO: remove temp table after it's done.
-//@TODO: need a temp way to keep old uid to map all old fields to new uid
+
+// db_query("DROP TABLE IF EXISTS " . $user_table);
