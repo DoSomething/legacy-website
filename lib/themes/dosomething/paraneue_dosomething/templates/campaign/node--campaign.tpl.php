2b618116814c46cc0e952994f7f157b3ab79c4ce
David Furnes <dfurnes@dosomething.org>
2014-03-10 16:49:18 -0400

    Added hooks for "highlight" code.

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
@@ -21,10 +21,10 @@
   <div class="content-wrapper">
     <nav id="nav" class="navigation js-sticky">
       <ul>
-        <li><a class="plain js-jump-scroll" href="#know">know</a></li>
-        <li><a class="plain js-jump-scroll" href="#do">do</a></li>
-        <li><a class="plain js-jump-scroll" href="#plan">plan</a></li>
-        <li><a class="primary js-jump-scroll" href="#prove">prove it</a></li>
+        <li><a class="plain js-jump-scroll js-highlight-scroll" href="#know">know</a></li>
+        <li><a class="plain js-jump-scroll js-highlight-scroll" href="#do">do</a></li>
+        <li><a class="plain js-jump-scroll js-highlight-scroll" href="#plan">plan</a></li>
+        <li><a class="primary js-jump-scroll js-highlight-scroll" href="#prove">prove it</a></li>
       </ul>
     </nav>
 
