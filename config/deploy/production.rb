6793d8a079fbaf040fa2dd79063ac14f02e63462
Desmond Morris <hi@desmondmorris.com>
2014-03-12 14:22:29 -0400

    Adds stub for production tasks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,37 +1,18 @@
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-# Don't declare `role :all`, it's a meta role
-role :app, %w{ubuntu@10.241.0.27}
+set :deploy_to, "/var/www/beta.dosomething.org"
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
-server '10.241.0.27', user: 'ubuntu', roles: %w{app}
+role :app, %w{dosomething@98.129.111.169}
 
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
+server '98.129.111.169', user: 'dosomething', roles: %w{app}
+
+namespace :deploy do
+
+  task :shared_links do
+    on roles(:app) do |host|
+      execute "cd '#{release_path}/html/sites/default'; rm -rf files 2> /dev/null; ln -s #{shared_path}/files files"
+      execute "ln -s #{shared_path}/settings.production.php #{release_path}/html/sites/default/settings.production.php"
+    end
+  end
+
+  after :build, 'deploy:shared_links'
+
+end
