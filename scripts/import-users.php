a8a0630fe4472fcad768af51c0e7dedcc7db4415
Andrea Gaither <angaither@gmail.com>
2014-03-13 14:38:43 -0400

    Updated user import script. - uids are set based on old uids - all active users are ported over

--- scripts/import-users.php
+++ scripts/import-users.php
@@ -4,34 +4,35 @@
  *
  * to run:
  * import user data from old world site into temporary tables
- * will need 'users', 'profile', and 'field_data_field_user_mobile'.
+ * will need legacy 'users', 'profile', and 'field_data_field_user_mobile' and 'field_data_field_user_birthday'.
+ * once temp tables are in place
  * drush --script-path=../scripts/ php-script import-users.php
  *
  */
-
-$user_table = '_users_old_world';
+$user_table = 'users';
+$old_users = '_users_old_world';
 $profile_id = '_profile_old_world';
 $mobile_data = '_mobile_old_world';
 $birthday_data = '_birthday_old_world';
 
-$result = db_query('SELECT * FROM {' . $user_table . '}');
+$result = db_query('SELECT * FROM {' . $old_users . '}');
 
 foreach ($result as $user_row) {
   // Only migrate users with active status.
-  if ($user_row->status == 1) {
-    $user = array();
-    // Will be overridden by uid.
-    $user['name'] = user_password(8);
-    $user['pass'] = $user_row->pass;
-    $user['mail'] = $user_row->mail;
-    $user['created'] = $user_row->created;
-    $user['access'] = $user_row->access;
-    // Set the init value as the old world uid.
-    $user['init']= $user_row->uid;
-    $user['status'] = $user_row->status;
-    $account = user_save('', $user);
+ if ($user_row->status == 1) {
+     $user = array();
+     // Will be overridden by uid.
+     $user['name'] = user_password(8);
+     $user['uid'] = $user_row->uid;
+     $user['pass'] = $user_row->pass;
+     $user['mail'] = $user_row->mail;
+     $user['created'] = $user_row->created;
+     $user['access'] = $user_row->access;
+     // Set the init value as the old world uid.
+     $user['init']= $user_row->uid;
+     $user['status'] = $user_row->status;
+     $account = user_save('', $user);
   }
-
   $edit = array();
   // Some users only have mobile as login method.
   $mobile_result = db_query("SELECT  m.field_user_mobile_value
@@ -71,5 +72,3 @@ foreach ($result as $user_row) {
 
 }
 
-
-// db_query("DROP TABLE IF EXISTS " . $user_table);
