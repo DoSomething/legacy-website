4cf262a3644266566c759eb3ed2d2d3b323a8682
Andrea Gaither <angaither@gmail.com>
2014-03-14 16:29:23 -0400

    Set the mind on my money nids in the settings file. Allows it to be used in other modules. yyeeeee. 🌴👙

--- config/settings.php
+++ config/settings.php
@@ -66,6 +66,10 @@ $conf['404_fast_paths_exclude'] = '/\/(?:styles)\//';
 $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
+/**
+ * Setting the beta and legacy running campaign nids.
+ */
+$conf['MOMM'] = array('legacy' =>731098, 'beta' => 850);
 
 /**
  * Load environment aware settings override files
