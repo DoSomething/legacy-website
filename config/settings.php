485c1ee3e3a85319b8187c17c1d7ad4e84dbcb28
Desmond Morris <hi@desmondmorris.com>
2014-03-16 14:23:59 -0400

    Adds redis config to settings file

--- config/settings.php
+++ config/settings.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * Lets do something about globals
+ */
+define('DS_PROFILE_PATH', 'profiles/dosomething');
+define('DS_MODULES_PATH', DS_PROFILE_PATH . '/modules');
+define('DS_THEMES_PATH', DS_PROFILE_PATH . 'themes');
+define('DS_LIBRARIES_PATH', DS_PROFILE_PATH . '/libraries');
+
 /**
  * Database settings:
  */
@@ -51,6 +60,29 @@ $conf['varnish_control_key'] = '00c9203c65874ca5b4c359e19f00bf56';
 // Drupal 7 does not cache pages when we invoke hooks during bootstrap.
 // This needs to be disabled.
 $conf['page_cache_invoke_hooks'] = FALSE;
+
+require_once DS_MODULES_PATH . '/contrib/redis/redis.autoload.inc';
+
+// Predis autoloader not working. Declare here instead.
+spl_autoload_register(function($classname) {
+  if (0 === strpos($classname, 'Predis\\')) {
+    $filename = DS_LIBRARIES_PATH . '/predis/lib/';
+    $filename .= str_replace('\\', '/', $classname) . '.php';
+    return (bool)require_once $filename;
+  }
+  return false;
+});
+
+$conf['cache_backends'][] = DS_MODULES_PATH . '/contrib/redis/redis.autoload.inc';
+$conf['redis_client_interface'] = 'Predis';
+$conf['redis_client_host'] = getenv('DS_REDIS_HOST') ?: '127.0.0.1';
+$conf['redis_client_port'] = getenv('DS_REDIS_PORT') ?: '6379';
+$conf['cache_class_cache'] = 'Redis_Cache';
+$conf['cache_class_cache_menu'] = 'Redis_Cache';
+$conf['cache_class_cache_bootstrap'] = 'Redis_Cache';
+
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['lock_inc'] = DS_MODULES_PATH . '/contrib/redis/redis.lock.inc';
 
 /**
  * Salt hash:
