213018bfc44e858f33a4d219f6ee2579bd8f2f52
David Furnes <dfurnes@dosomething.org>
2014-03-17 01:07:17 -0400

    Move HTML5Shiv to Paraneue.

--- lib/themes/dosomething/paraneue_dosomething/template.php
+++ lib/themes/dosomething/paraneue_dosomething/template.php
@@ -10,7 +10,6 @@ define('PARANEUE_DS_PATH', drupal_get_path('theme', 'paraneue_dosomething'));
  * Implements hook_preprocess_page().
  */
 function paraneue_dosomething_preprocess_page(&$vars, $hook) {
-  paraneue_dosomething_preprocess_page_html5shiv($vars, $hook);
 }
 
 /**
