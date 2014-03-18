778152ef34d98d1868c9902186cfb693e7da08f9
David Furnes <dfurnes@dosomething.org>
2014-03-18 00:53:06 -0400

    Updated template to match homepage content type.

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/node--home.tpl.php
@@ -0,0 +1,58 @@
+  <div class="homepage--hero">
+    <div class="header">
+      <h1>Join over 2.5 million young people taking action.</h1>
+      <p>Make the world suck less. Pick a campaign below to get started.</p>
+    </div>
+  </div>
+
+  <div class="homepage--grid">
+  
+    <div class="block-big" style="background-image: url('http://www.dosomething.org/files/u/neue-homepage/pbj-featured.jpg'); background-position: center center;">
+      <a class="full-link" href="/pbj"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Collect peanut butter for your local food bank.</h2>
+      </div>
+    </div>
+
+    <div class="block-small" style="background-image: url('http://www.dosomething.org//files/u/neue-homepage/wyr-small.jpg'); background-position: top center;">
+      <a class="full-link" href="/wyr"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Challenge your friends to a game of "Would You Rather?"</h2>
+      </div>
+    </div>
+
+    <div class="block-small" style="background-image: url('http://www.dosomething.org//files/u/neue-homepage/pregtext.jpg'); background-position: center center;">
+      <a class="full-link" href="/pregnancytext"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Impregnate your friend's phones!</h2>
+      </div>
+    </div>
+
+    <div class="block-small" style="background-image: url('http://www.dosomething.org//files/u/neue-homepage/bullytext-block.jpg'); background-position: center center;">
+      <a class="full-link" href="/bully"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Everyone has the power to stop bullying. Learn how.</h2>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div class="homepage--sponsors">
+    <h4>Sponsors</h4>
+    <p>
+      <i title="H&amp;R Block" class="sprite sprite-HRB"></i>
+      <i title="Aeropostale" class="sprite sprite-aeropostale"></i>
+      <i title="Channel One" class="sprite sprite-channel-one"></i>
+      <i title="Fastweb" class="sprite sprite-fastweb"></i>
+      <i title="Toyota" class="sprite sprite-toyota"></i>
+      <i title="JetBlue" class="sprite sprite-jetblue"></i>
+      <i title="AARP" class="sprite sprite-aarp"></i>
+      <i title="Sprint" class="sprite sprite-sprint"></i>
+      <i title="H&amp;M" class="sprite sprite-hm"></i>
+      <i title="Salt" class="sprite sprite-salt"></i>
+      <i title="American Express" class="sprite sprite-amex"></i>
+      <i title="Google" class="sprite sprite-google"></i>
+    </p>
+  </div>
+   
