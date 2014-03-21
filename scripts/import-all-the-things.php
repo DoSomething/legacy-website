96155f500f47c06d11bdd8595b40934520a22e6a
Andrea Gaither <angaither@gmail.com>
2014-03-21 12:38:45 -0400

    Updated the other firstname and birthdate script to run faster.

--- scripts/import-all-the-things.php
+++ scripts/import-all-the-things.php
@@ -18,52 +18,45 @@ $first_name = '_first_name_old_world';
 $users = 'users';
 
 // Let's only try to get people who don't already have a mobile value.
-$users_no_data = db_query("SELECT uid FROM {$users} u
+$users_no_data = db_query("SELECT u.uid, ob.field_user_birthday_value, ofn.field_user_first_name_value
+                    FROM {$users} u
                     LEFT JOIN field_data_field_first_name fn ON fn.entity_id = u.uid
                     LEFT JOIN field_data_field_birthdate b on b.entity_id = u.uid
+                    INNER JOIN {$profile_id} p on p.uid = u.uid
+                    LEFT JOIN {$birthday_data} ob on ob.entity_id = p.pid
+                    LEFT JOIN {$first_name} ofn on ofn.entity_id = p.pid
                     WHERE fn.field_first_name_value is NULL
                     OR b.field_birthdate_value is NULL;");
-$edit = array();
+
 foreach ($users_no_data as $user_row) {
-  $account = user_load($user_row->uid);
-
-  $birthday_result = db_query("SELECT  b.field_user_birthday_value
-                               FROM {$birthday_data} b
-                               INNER JOIN {$profile_id} p on b.entity_id = p.pid
-                               WHERE p.uid = $user_row->uid")->fetchField();
-
-  if ($birthday_result) {
+  $edit = array();
+  if ($user_row->field_user_birthday_value) {
     $edit['field_birthdate'] = array(
       LANGUAGE_NONE => array(
         0 => array(
-          'value' => $birthday_result,
+          'value' => $user_row->field_user_birthday_value,
         ),
       ),
     );
   }
 
-  $first_name_result = db_query("SELECT fn.field_user_first_name_value
-                                 FROM {$first_name} fn
-                                 INNER JOIN {$profile_id} p on fn.entity_id = p.pid
-                                 WHERE p.uid = $user_row->uid")->fetchField();
-
-
-  if ($first_name_result) {
+  if ($user_row->field_user_first_name_value) {
     $edit['field_first_name'] = array(
       LANGUAGE_NONE => array(
         0 => array(
-          'value' => $first_name_result,
+          'value' => $user_row->field_user_first_name_value,
         ),
       ),
     );
   }
   if (!empty($edit)) {
+    $account = user_load($user_row->uid);
     // Save account with extra data.
-      user_save($account, $edit);
+    user_save($account, $edit);
 
-      // Calculate the age if we have the birthday.
-      if (in_array('field_birthdate', $edit)) {
-        dosomething_user_is_under_thirteen($account);
-      }
+    // Calculate the age if we have the birthday.
+    if (in_array('field_birthdate', $edit)) {
+      dosomething_user_is_under_thirteen($account);
     }
   }
+}
