cd54c112be7846451778b6eba154276f971e5267
Andrea Gaither <angaither@gmail.com>
2014-03-21 10:47:41 -0400

    Update comments

--- scripts/import-mobile-data.php
+++ scripts/import-mobile-data.php
@@ -15,17 +15,18 @@ $profile_id = '_profile_old_world';
 $mobile_data = '_mobile_old_world';
 $users = 'users';
 
-// Let's only try to get people who don't already have a mobile value.
+// Only select users who don't have mobile in the new world and have mobile in the old world.
 $users_no_mobile = db_query("SELECT u.uid, mbw.field_user_mobile_value
                     FROM {$users} u
                     LEFT JOIN field_data_field_mobile m ON m.entity_id = u.uid
                     INNER JOIN {$profile_id} p on p.uid = u.uid
                     LEFT JOIN {$mobile_data} mbw ON mbw.entity_id = p.pid
-                    WHERE m.field_mobile_value is NULL AND mbw.field_user_mobile_value != '';");
+                    WHERE m.field_mobile_value is NULL
+                    AND mbw.field_user_mobile_value != '';");
 
 $edit = array();
 foreach ($users_no_mobile as $user_row) {
-
+  // If there is a mobile value.
   if ($user_row->field_user_mobile_value) {
     $edit['field_mobile'] = array(
       LANGUAGE_NONE => array(
@@ -34,6 +35,7 @@ foreach ($users_no_mobile as $user_row) {
         ),
       ),
     );
+    // Load the user before the save.
     $account = user_load($user_row->uid);
     // Save account with extra data.
     user_save($account, $edit);
