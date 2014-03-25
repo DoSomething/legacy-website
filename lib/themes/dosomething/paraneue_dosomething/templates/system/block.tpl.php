c5fe621460c7960e0597fb475a35519334c08e6b
David Furnes <dfurnes@dosomething.org>
2014-03-25 18:37:46 -0400

    Merges Paraneue and Paraneue_DS.

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/block.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Returns the HTML for a block.
+ * @see https://drupal.org/node/1728246
+ */
+?>
+
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+<?php if ($block->subject): ?>
+  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+<?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print $content ?>
+  </div>
+</div>
---
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/node.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Returns the HTML for a node.
+ * @see https://drupal.org/node/1728164 
+ */
+?>
+
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+</article>
