76dd0e3f1aee00734155d2561105a14d5cab7ae9
Diego Lorenzo <dlorenzo@dosomething.org>
2014-04-02 11:04:42 -0400

    Adding initial template file and associated sass file.

--- lib/themes/dosomething/paraneue_dosomething/templates/user/user-profile.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/user/user-profile.tpl.php
@@ -2,10 +2,29 @@
 /**
  * Returns the HTML for User Profile page.
  * @see https://api.drupal.org/api/drupal/modules%21user%21user-profile.tpl.php/7
+ *
+ * Available Variables
+ * - $user_profile: An array of profile items.
+ *   - [campaigns]: A render array item.
+ *   - [user_picture]: A render array item.
+ *   - [field_first_name]: A render array item.
+ *   - [field_last_name]: A render array item.
+ *   - [field_address]: A render array item.
+ *   - [field_birthday]: A render array item.
+ *   - [field_under_thirteen]: A render array item.
+ *   - [field_mobile]: A render array item.
+ *   - [field_partner]: A render array item.
+ *   - [summary]: A render array item.
  */
+
+// krumo($variables);
+// krumo($user_profile);
 ?>
 
-<div class="profile"<?php print $attributes; ?>>
-  <h1>USER</h1>
-  <?php print render($user_profile); ?>
-</div>
+<article>
+
+  <div class="user profile"<?php print $attributes; ?>>
+    <?php print render($user_profile); ?>
+  </div>
+
+</article>
