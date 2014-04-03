53725662fdc8fba25b523dce0fb3c1def6cad9cd
David Furnes <dfurnes@dosomething.org>
2014-04-03 16:12:06 -0400

    Disabling campaign-finder stuff so I can merge in.

--- lib/themes/dosomething/paraneue_dosomething/templates/home/node--home.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/home/node--home.tpl.php
@@ -6,92 +6,52 @@
     </div>
   </div>
 
-  <div class="campaign-finder">
-    <div class="dropdown toggle-lg toggle">
-      <div class="caret-toggle facet-field" data-toggle="dropdown">Cause</div>
-      <p class="facet-field">What are you passionate about?</p>
-      <div class="dropdown-menu">
-        <ul class="two-col">
-          <li><label><input name="cause" type="checkbox" value="16" />Animals</label></li>
-          <li><label><input name="cause" type="checkbox" value="13 OR 17" />Bullying + Violence</label></li>
-          <li><label><input name="cause" type="checkbox" value="12" />Disasters</label></li>
-          <li><label><input name="cause" type="checkbox" value="14" />Discrimination</label></li>
-          <li><label><input name="cause" type="checkbox" value="2" />Education</label></li>
-          <li><label><input name="cause" type="checkbox" value="4" />Environment</label></li>
-          <li><label><input name="cause" type="checkbox" value="19 OR 5" />Health</label></li>
-          <li><label><input name="cause" type="checkbox" value="6 OR 15" />Homelessness + Poverty</label></li>
-          <li><label><input name="cause" type="checkbox" value="1 OR 21" />Sex + Relationships</label></li>
-        </ul>
+  <div class="homepage--grid">
+
+    <div class="block-big" style="background-image: url('//www.dosomething.org/files/u/home/ComebackClothes_homepage_landscape.jpg'); background-position: center center;">
+      <a class="full-link" href="/campaigns/comeback-clothes"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Recycle old or worn-out clothes to help our planet.</h2>
       </div>
     </div>
 
-    <div class="dropdown toggle-sm toggle">
-      <div class="caret-toggle facet-field" data-toggle="dropdown">Time</div>
-      <p class="facet-field">How long do you have?</p>
-      <div class="dropdown-menu">
-        <ul>
-          <li><label><input name="time" type="checkbox" value="[* TO 2]" />Less than 2 hours</label></li>
-          <li><label><input name="time" type="checkbox" value="[2 TO 5]" />5 hours</label></li>
-          <li><label><input name="time" type="checkbox" value="[5 TO *]" />10 hours</label></li>
-        </ul>
+    <div class="block-small" style="background-image: url('//www.dosomething.org/files/u/neue-homepage/wyr-small.jpg'); background-position: top center;">
+      <a class="full-link" href="/wyr"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Challenge your friends to a game of "Would You Rather?"</h2>
       </div>
     </div>
 
-    <div class="dropdown toggle-lg toggle">
-      <div class="caret-toggle facet-field" data-toggle="dropdown">Type</div>
-      <p class="facet-field">What would you like to do?</p>
-      <div class="dropdown-menu">
-        <ul class="two-col">
-          <li><label><input name="action-type" type="checkbox" value="7" />Donate Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="3" />Face to Face</label></li>
-          <li><label><input name="action-type" type="checkbox" value="11" />Host an Event</label></li>
-          <li><label><input name="action-type" type="checkbox" value="8" />Improve a Space</label></li>
-          <li><label><input name="action-type" type="checkbox" value="9" />Make Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="18" />Share Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="10" />Start Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="20" />Take a Stand</label></li>
-        </ul>
+    <div class="block-small" style="background-image: url('//www.dosomething.org/files/u/neue-homepage/pbj-small.jpg'); background-position: center center;">
+      <a class="full-link" href="/campaigns/peanut-butter-jam-slam"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Collect peanut butter for your local food bank.</h2>
       </div>
     </div>
 
-    <div class="campaign-search">
-      <button>Find a Campaign</button>
+    <div class="block-small" style="background-image: url('//www.dosomething.org/files/u/neue-homepage/bullytext-block.jpg'); background-position: center center;">
+      <a class="full-link" href="/bully"><span>Do This</span></a>
+      <div class="headline">
+        <h2>Everyone has the power to stop bullying. Learn how.</h2>
+      </div>
     </div>
   </div>
 
-  <div id="campaign-results"></div>
-
-
-  <div id="campaign-finder-template" class="template">
-    <a class="campaign-result">
-      <div class="flag staff-pick">Staff Pick</div>
-      <div class="headline-wrapper">
-        <div class="headline">
-          <h3></h3>
-          <p></p>
-          <div class="powered-by">Powered by <img /></div>
-        </div>
-      </div>
-    </a>
+  <div class="homepage--sponsors">
+    <h4>Sponsors</h4>
+    <p>
+      <i title="H&amp;R Block" class="sprite sprite-HRB"></i>
+      <i title="Aeropostale" class="sprite sprite-aeropostale"></i>
+      <i title="Channel One" class="sprite sprite-channel-one"></i>
+      <i title="Fastweb" class="sprite sprite-fastweb"></i>
+      <i title="Toyota" class="sprite sprite-toyota"></i>
+      <i title="JetBlue" class="sprite sprite-jetblue"></i>
+      <i title="AARP" class="sprite sprite-aarp"></i>
+      <i title="Sprint" class="sprite sprite-sprint"></i>
+      <i title="H&amp;M" class="sprite sprite-hm"></i>
+      <i title="Salt" class="sprite sprite-salt"></i>
+      <i title="American Express" class="sprite sprite-amex"></i>
+      <i title="Google" class="sprite sprite-google"></i>
+    </p>
   </div>
-
-
-</div>
-
-<div class="homepage--sponsors">
-  <h4>Sponsors</h4>
-  <p>
-    <i title="H&amp;R Block" class="sprite sprite-HRB"></i>
-    <i title="Aeropostale" class="sprite sprite-aeropostale"></i>
-    <i title="Channel One" class="sprite sprite-channel-one"></i>
-    <i title="Fastweb" class="sprite sprite-fastweb"></i>
-    <i title="Toyota" class="sprite sprite-toyota"></i>
-    <i title="JetBlue" class="sprite sprite-jetblue"></i>
-    <i title="AARP" class="sprite sprite-aarp"></i>
-    <i title="Sprint" class="sprite sprite-sprint"></i>
-    <i title="H&amp;M" class="sprite sprite-hm"></i>
-    <i title="Salt" class="sprite sprite-salt"></i>
-    <i title="American Express" class="sprite sprite-amex"></i>
-    <i title="Google" class="sprite sprite-google"></i>
-  </p>
 </div>
