613475da7c7277be5ef2e60d46085114e46daca0
David Furnes <dfurnes@dosomething.org>
2014-04-08 11:23:49 -0400

    Use link text for banner title.

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
@@ -223,6 +223,7 @@
 
       <?php if (isset($signup_data_form)): ?>
       <script id="modal-signup-data-form" type="text/cached-modal">
+        <h2 class="banner"><?php print $signup_data_form_; ?></h2>
         <a href="#" class="js-close-modal modal-close-button white">×</a>
           <div><?php print render($signup_data_form); ?></div>
         <a href="#" class="js-close-modal">Back to main page</a>
