80e4370f126e28389c07c63733d6ce7c61155da6
David Furnes <dfurnes@dosomething.org>
2014-04-08 02:42:44 -0400

    Design fixes, simplifies tile rendering.

--- lib/themes/dosomething/paraneue_dosomething/templates/home/node--home.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/home/node--home.tpl.php
@@ -10,63 +10,63 @@ if( theme_get_setting('show_campaign_finder') ) {
 ?>
 
 <div class="home--wrapper">
-  <div class="home--hero">
-    <div class="header">
-      <h1>What do you want to do today?</h1>
-    </div>
-  </div>
-
-
   <div class="campaign-finder js-campaign-finder">
-    <div class="dropdown toggle-lg toggle">
-      <div class="caret-toggle facet-field" data-toggle="dropdown">Cause</div>
-      <p class="facet-field">What are you passionate about?</p>
-      <div class="dropdown-menu">
-        <ul class="two-col">
-          <li><label><input name="cause" type="checkbox" value="16" />Animals</label></li>
-          <li><label><input name="cause" type="checkbox" value="13 OR 17" />Bullying + Violence</label></li>
-          <li><label><input name="cause" type="checkbox" value="12" />Disasters</label></li>
-          <li><label><input name="cause" type="checkbox" value="14" />Discrimination</label></li>
-          <li><label><input name="cause" type="checkbox" value="2" />Education</label></li>
-          <li><label><input name="cause" type="checkbox" value="4" />Environment</label></li>
-          <li><label><input name="cause" type="checkbox" value="19 OR 5" />Health</label></li>
-          <li><label><input name="cause" type="checkbox" value="6 OR 15" />Homelessness + Poverty</label></li>
-          <li><label><input name="cause" type="checkbox" value="1 OR 21" />Sex + Relationships</label></li>
-        </ul>
+    <div class="home--hero">
+      <div class="header">
+        <h1>What do you wanna do <span class="underline">today</span>?</h1>
       </div>
-    </div>
+      <div class="header">
+        <div class="dropdown toggle-lg toggle">
+          <div class="caret-toggle facet-field" data-toggle="dropdown">Cause</div>
+          <p class="facet-field">What are you passionate about?</p>
+          <div class="dropdown-menu">
+            <ul class="two-col">
+              <li><label><input name="cause" type="checkbox" value="16" />Animals</label></li>
+              <li><label><input name="cause" type="checkbox" value="13 OR 17" />Bullying + Violence</label></li>
+              <li><label><input name="cause" type="checkbox" value="12" />Disasters</label></li>
+              <li><label><input name="cause" type="checkbox" value="14" />Discrimination</label></li>
+              <li><label><input name="cause" type="checkbox" value="2" />Education</label></li>
+              <li><label><input name="cause" type="checkbox" value="4" />Environment</label></li>
+              <li><label><input name="cause" type="checkbox" value="19 OR 5" />Health</label></li>
+              <li><label><input name="cause" type="checkbox" value="6 OR 15" />Homelessness + Poverty</label></li>
+              <li><label><input name="cause" type="checkbox" value="1 OR 21" />Sex + Relationships</label></li>
+            </ul>
+          </div>
+        </div>
 
-    <div class="dropdown toggle-sm toggle">
-      <div class="caret-toggle facet-field" data-toggle="dropdown">Time</div>
-      <p class="facet-field">How long do you have?</p>
-      <div class="dropdown-menu">
-        <ul>
-          <li><label><input name="time" type="checkbox" value="[* TO 2]" />Less than 2 hours</label></li>
-          <li><label><input name="time" type="checkbox" value="[2 TO 5]" />5 hours</label></li>
-          <li><label><input name="time" type="checkbox" value="[5 TO *]" />10 hours</label></li>
-        </ul>
+        <div class="dropdown toggle-sm toggle">
+          <div class="caret-toggle facet-field" data-toggle="dropdown">Time</div>
+          <p class="facet-field">How long do you have?</p>
+          <div class="dropdown-menu">
+            <ul>
+              <li><label><input name="time" type="checkbox" value="[* TO 2]" />Less than 2 hours</label></li>
+              <li><label><input name="time" type="checkbox" value="[2 TO 5]" />5 hours</label></li>
+              <li><label><input name="time" type="checkbox" value="[5 TO *]" />10 hours</label></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="dropdown toggle-lg toggle">
+          <div class="caret-toggle facet-field" data-toggle="dropdown">Type</div>
+          <p class="facet-field">What would you like to do?</p>
+          <div class="dropdown-menu">
+            <ul class="two-col">
+              <li><label><input name="action-type" type="checkbox" value="7" />Donate Something</label></li>
+              <li><label><input name="action-type" type="checkbox" value="3" />Face to Face</label></li>
+              <li><label><input name="action-type" type="checkbox" value="11" />Host an Event</label></li>
+              <li><label><input name="action-type" type="checkbox" value="8" />Improve a Space</label></li>
+              <li><label><input name="action-type" type="checkbox" value="9" />Make Something</label></li>
+              <li><label><input name="action-type" type="checkbox" value="18" />Share Something</label></li>
+              <li><label><input name="action-type" type="checkbox" value="10" />Start Something</label></li>
+              <li><label><input name="action-type" type="checkbox" value="20" />Take a Stand</label></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="campaign-search">
+          <button>Find a Campaign</button>
+        </div>
       </div>
-    </div>
-
-    <div class="dropdown toggle-lg toggle">
-      <div class="caret-toggle facet-field" data-toggle="dropdown">Type</div>
-      <p class="facet-field">What would you like to do?</p>
-      <div class="dropdown-menu">
-        <ul class="two-col">
-          <li><label><input name="action-type" type="checkbox" value="7" />Donate Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="3" />Face to Face</label></li>
-          <li><label><input name="action-type" type="checkbox" value="11" />Host an Event</label></li>
-          <li><label><input name="action-type" type="checkbox" value="8" />Improve a Space</label></li>
-          <li><label><input name="action-type" type="checkbox" value="9" />Make Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="18" />Share Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="10" />Start Something</label></li>
-          <li><label><input name="action-type" type="checkbox" value="20" />Take a Stand</label></li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="campaign-search">
-      <button>Find a Campaign</button>
     </div>
   </div>
 
