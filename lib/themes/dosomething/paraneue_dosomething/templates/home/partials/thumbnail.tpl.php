365f35deac08eb09dfa91c574ee37dd6c5307ca5
David Furnes <dfurnes@dosomething.org>
2014-04-16 16:23:59 -0400

    Generate homepage from an array of NIDs.

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/home/partials/thumbnail.tpl.php
@@ -0,0 +1,11 @@
+<div class="campaign-result <?php print $modifier_classes ?>">
+  <?php if($staff_pick) { ?>
+    <div class="flag staff-pick">Staff Pick</div>
+  <?php } ?>
+  <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=" data-src="<?php print $image ?>">
+  <div class="headline">
+    <h3><?php print $title; ?></h3>
+    <p><?php print $cta; ?></p>
+  </div>
+  <?php print $link; ?>
+</div>
