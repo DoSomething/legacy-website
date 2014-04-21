519fba8aa7f470898cfd35614b7c1adecbb0c406
Diego Lorenzo <dlorenzo@dosomething.org>
2014-04-21 16:23:47 -0400

    Progress on User Profile stuff.

--- lib/themes/dosomething/paraneue_dosomething/templates/user/user-profile.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/user/user-profile.tpl.php
@@ -4,79 +4,98 @@
  * @see https://api.drupal.org/api/drupal/modules%21user%21user-profile.tpl.php/7
  *
  * Available Variables
- * - $user_profile: An array of profile items.
- *   - [campaigns]: A render array item.
- *   - [user_picture]: A render array item.
- *   - [field_first_name]: A render array item.
- *   - [field_last_name]: A render array item.
- *   - [field_address]: A render array item.
- *   - [field_birthday]: A render array item.
- *   - [field_under_thirteen]: A render array item.
- *   - [field_mobile]: A render array item.
- *   - [field_partner]: A render array item.
- *   - [summary]: A render array item.
- *
- *   - [user_info]: An array of profile information that only contains the values.
- *     - [id]: User id number (string).
- *     - [username]: Account name (string).
- *     - [email]: Account email address (string).
- *     - [first_name]: User's first name (string).
- *     - [last_name]: User's last name (string).
- *     - [birthday]: User's birthday (string).
- *     - [address]: Array containing address information.
- *       - [country]: User's country of residence (string).
- *       - [state]: User's state of residence (string).
- *       - [city]: User's city of residence (string).
- *       - [zip]: User's postal zip code (string).
- *       - [street]: User's street address (string).
- *       - [premise]: User's apartment, suite, etc... (string).
+ * - $user_account: An array of user account profile information.
+ *   - [id]: User id number (string).
+ *   - [username]: Account name (string).
+ *   - [email]: Account email address (string).
+ *   - [first_name]: User's first name (string).
+ *   - [last_name]: User's last name (string).
+ *   - [birthday]: User's birthday (string).
+ *   - [address]: Array containing address information.
+ *     - [country]: User's country of residence (string).
+ *     - [state]: User's state of residence (string).
+ *     - [city]: User's city of residence (string).
+ *     - [zip]: User's postal zip code (string).
+ *     - [street]: User's street address (string).
+ *     - [premise]: User's apartment, suite, etc... (string).
+ *   - [mobile]: User's mobile cell number (string).
+ *   - [campaigns]: Array containing a data for campaigns a User has signed up for.
+ *   - [recommended]: Array containing a data for recommended campaigns for User.
  */
 
 
-  $user_info = $user_profile['user_info'];
-
   // krumo($variables);
-  // krumo($user_profile);
-  // krumo($user_info);
+  krumo($user_profile);
+  krumo($user_account);
+
+  $recommended = $user_account['recommended'];
+  $campaigns = $user_account['campaigns'];
 ?>
 
 
-<article id="user-<?php print $user_info['id']; ?>" class="user profile"<?php print $attributes; ?>>
+<article id="user-<?php print $user_account['id']; ?>" class="user profile"<?php print $attributes; ?>>
   <?php //print render($user_profile); ?>
 
+  <?php //@TODO: Refactor markup to add wrapper inside header tag, when all headers are refactored. ?>
   <div class="header-wrapper">
     <header class="header">
-      <h1 class="title">Hey, <?php print $user_info['first_name']; ?>!</h1>
+      <h1 class="title">Hey, <?php print $user_account['first_name']; ?>!</h1>
       <h2 class="subtitle">Let us get to know you better.</h2>
     </header>
   </div>
 
-  <div class="profile-info-wrapper">
-    <section>
-      <h2>My Campaigns</h2>
-        <p>Rut Roh! You haven't signed up for any campaigns yet. Find something to do:</p>
-      <h3>Signed Up</h3>
+  <section class="profile--campaigns">
+    <h1 class="banner"><span>My Campaigns</span></h1>
 
-      <h3>Recommended</h3>
-    </section>
+    <?php if(!$campaigns): ?>
+      <div class="cta">
+        <h2>Rut Roh! You haven't signed up for any campaigns yet. Find something to do:</h2>
+        <a href="/campaigns" class="btn medium">Explore Campaigns</a>
+      </div>
+    <?php endif; ?>
 
-    <section>
-      <h2>My Settings</h2>
+    <div class="wrapper">
+      <?php if($campaigns): ?>
+          <h2>Signed Up</h2>
+          <?php print render($campaigns); ?>
+      <?php endif; ?>
 
-      <p><span>Name:</span> <?php print $user_info['first_name']; ?>  <?php print $user_info['last_name']; ?></p>
-      <p><span>Email:</span> <?php print $user_info['email']; ?></p>
-      <p><span>Mobile:</span> <?php print $user_info['mobile']; ?></p>
-      <p><span>Birthday:</span> <?php print $user_info['birthday']; ?></p>
+      <?php if ($recommended): ?>
+        <h2>Recommended</h2>
+        <ul>
+          <?php foreach($recommended as $index => $campaign): ?>
+            <li>
+              <article class="campaign--teaser" id="campaign-<?php print $campaign['nid']; ?>">
+                <?php //print l($campaign['title'], $campaign['src']); ?>
+                <?php print $campaign['call_to_action']; ?>
+                <img alt="<?php //print $campaign['title']; ?>" src="<?php print $campaign['image']; ?>" />
+              </article>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
+    </div>
+  </section>
 
-      <div class="address">
-        <p><span>Address 1:</span> <?php print $user_info['address']['street']; ?></p>
-        <p><span>Address 2:</span> <?php print $user_info['address']['premise']; ?></p>
-        <p><span>City, State, Zip:</span> <?php print $user_info['address']['city']; ?>, <?php print $user_info['address']['state']; ?>, <?php print $user_info['address']['zip']; ?></p>
+  <section class="profile--settings">
+    <h1 class="banner"><span>My Settings</span></h1>
+
+    <div class="wrapper">
+      <div class="__user-info">
+        <p><strong>Name:</strong> <?php print $user_account['first_name']; ?>  <?php print $user_account['last_name']; ?></p>
+        <p><strong>Email:</strong> <?php print $user_account['email']; ?></p>
+        <p><strong>Mobile:</strong> <?php print $user_account['mobile']; ?></p>
+        <p><strong>Birthday:</strong> <?php print $user_account['birthday']; ?></p>
       </div>
 
-      <?php print render($user_profile['updated']); ?>
-    </section>
+      <div class="__address-info">
+        <p><strong>Address 1:</strong> <?php print $user_account['address']['street']; ?></p>
+        <p><strong>Address 2:</strong> <?php print $user_account['address']['premise']; ?></p>
+        <p><strong>City, State, Zip:</strong> <?php print $user_account['address']['city']; ?>, <?php print $user_account['address']['state']; ?>, <?php print $user_account['address']['zip']; ?></p>
+      </div>
+    </div>
 
-  </div>
+    <?php print render($user_profile['update']); ?>
+  </section>
 
 </article>
