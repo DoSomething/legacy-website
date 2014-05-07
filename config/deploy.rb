d800ff0223fb3b29ae7c3be1f2b8e8e4698360c7
Desmond Morris <hi@desmondmorris.com>
2014-05-07 09:03:31 -0400

    Adds branch argument to enable passing of tags to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,12 @@ lock '3.1.0'
 
 set :application, 'beta.dosomething.org'
 set :repo_url, 'git@github.com:DoSomething/dosomething.git'
+
 set :branch, "dev"
+if ENV['branch']
+    set :branch, ENV['branch'] || 'dev'
+end
+
 set :ssh_options, { :forward_agent => true }
 
 # Default value for :linked_files is []
