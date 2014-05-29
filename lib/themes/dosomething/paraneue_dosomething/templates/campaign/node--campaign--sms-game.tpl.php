19099024a906d70bb6b6061623e4360ddb421525
Diego Lorenzo <dlorenzo@dosomething.org>
2014-05-29 13:39:33 -0400

    Configuring initial structural markup.

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--sms-game.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--sms-game.tpl.php
@@ -10,7 +10,7 @@
  */
 ?>
 
-<section class="campaign--wrapper action">
+<article id="" class="">
   <header role="banner" class="-hero <?php print $classes; ?>">
     <div class="wrapper">
       <h1 class="__title"><?php print $title; ?></h1>
@@ -35,16 +35,18 @@
     </div>
   </header>
 
-  <div class="content-wrapper">
 
-    <h2 id="know" class="banner"><span>Step 1: Know It</span></h2>
-    <section class="know step">
+  <section id="know" class="container know">
+    <h2 class="container__title banner"><span>Step 1: Know It</span></h2>
 
-      <div class="col first">
+    <div class="wrapper">
+      <div class="container__body">
+
         <h4 class="inline--alt-color">The Problem</h4>
-
         <?php if (isset($fact_problem)): ?>
-        <div class="fact-problem"><?php print $fact_problem['fact']; ?><sup><?php print $fact_problem['footnotes']; ?></sup></div>
+        <div class="fact-problem">
+          <?php print $fact_problem['fact']; ?><sup><?php print $fact_problem['footnotes']; ?></sup>
+        </div>
         <?php endif; ?>
 
         <?php if (isset($psa)): ?>
@@ -54,71 +56,74 @@
         <?php if (!isset($psa)): ?>
           <?php print $modals; ?>
         <?php endif; ?>
+
       </div>
 
-      <div class="col second">
-        <h4 class="inline--alt-color">The Solution</h4>
+      <h4 class="inline--alt-color">The Solution</h4>
 
-        <?php if (isset($fact_solution)): ?>
-          <div class="fact-solution"><?php print $fact_solution['fact']; ?><sup><?php print $fact_solution['footnotes']; ?></sup></div>
-        <?php elseif (isset($solution_copy)): ?>
-          <div class="solution-copy"><?php print $solution_copy['safe_value']; ?></div>
-        <?php endif; ?>
+      <?php if (isset($fact_solution)): ?>
+        <div class="fact-solution"><?php print $fact_solution['fact']; ?><sup><?php print $fact_solution['footnotes']; ?></sup></div>
+      <?php elseif (isset($solution_copy)): ?>
+        <div class="solution-copy"><?php print $solution_copy['safe_value']; ?></div>
+      <?php endif; ?>
 
-        <?php if (isset($solution_support)): ?>
-        <div class="solution-supporting-copy"><?php print $solution_support; ?></div>
-        <?php endif; ?>
+      <?php if (isset($solution_support)): ?>
+      <div class="solution-supporting-copy"><?php print $solution_support; ?></div>
+      <?php endif; ?>
 
-        <?php if (isset($psa)): ?>
-          <?php print $modals; ?>
-        <?php endif; ?>
-      </div>
+      <?php if (isset($psa)): ?>
+        <?php print $modals; ?>
+      <?php endif; ?>
 
       <?php if (isset($fact_sources)): ?>
-      <div class="col sources-wrapper">
-        <a href="#" class="js-toggle-sources secondary">Sources</a>
-        <div class="sources">
-          <div class="legal">
-            <?php foreach ($fact_sources as $key => $source): ?>
-              <div><sup><?php print ($key + 1); ?></sup> <?php print $source; ?></div>
-            <?php endforeach; ?>
-          </div>
+      <a href="#" class="js-toggle-sources secondary">Sources</a>
+      <div class="sources">
+        <div class="legal">
+          <?php foreach ($fact_sources as $key => $source): ?>
+            <div><sup><?php print ($key + 1); ?></sup> <?php print $source; ?></div>
+          <?php endforeach; ?>
         </div>
       </div>
       <?php endif; ?>
-    </section>
+    </div>
 
-    <h2 id="plan" class="banner"><span>Step 2: Share It</span></h2>
-    <section>
+  </section>
+
+  <section id="plan" class="container plan">
+    <h2 class="container__title banner"><span>Step 2: Share It</span></h2>
+
+    <div class="wrapper">
       <?php if (isset($starter)) : ?>
-        <div class="intro"><?php print $starter['safe_value']; ?></div>
+        <div><?php print $starter['safe_value']; ?></div>
       <?php endif; ?>
       <div>
         <?php print render($signup_form); ?>
       </div>
-    </section>
+    </div>
+  </section>
 
-      <footer class="help <?php isset($sponsors) ? print 'sponsors' : '' ; ?>">
-        <div class="footer-content">
-          <div class="help-wrapper">Have a question? <a href="#modal-help" class="js-modal-link secondary">Contact us</a></div>
-
-          <?php if (isset($sponsors)): ?>
-          <div class="sponsor-wrapper">
-            In partnership with
-              <?php print $formatted_partners; ?>
-          </div>
-          <?php endif; ?>
-        </div>
-      </footer>
+  <?php if (isset($zendesk_form) || isset($sponsors)): ?>
+  <footer class="info-bar">
+    <div class="wrapper">
 
       <?php if (isset($zendesk_form)): ?>
-      <script id="modal-help" type="text/cached-modal" data-modal-close="true" data-modal-close-class="white">
-        <h2 class="banner">Contact Us</h2>
-        <p>Enter your question below. Please be as specific as possible.</p>
-
-        <?php print render($zendesk_form); ?>
-      </script>
+      <div class="help">
+        Questions? <a href="#modal-contact-form" class="js-modal-link">Contact Us</a>
+        <script id="modal-contact-form" class="modal--contact" type="text/cached-modal" data-modal-close="true" data-modal-close-class="white">
+          <h2 class="banner">Contact Us</h2>
+          <p>Enter your question below. Please be as specific as possible.</p>
+          <?php print render($zendesk_form); ?>
+        </script>
+      </div>
       <?php endif; ?>
-    </section>
-  </div>
-</section>
+
+      <?php if (isset($sponsors)): ?>
+        <div class="sponsor">
+          In partnership with <?php print $formatted_partners; ?>
+        </div>
+      <?php endif; ?>
+    </div>
+  </footer>
+  <?php endif; ?>
+
+</article>
