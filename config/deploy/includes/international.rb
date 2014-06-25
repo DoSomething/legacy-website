57dd8be32b48d106c0454ea2b141f592f43f0ff4
Morgan Rich <mrich@dosomething.org>
2014-06-25 16:21:27 -0400

    Removing extra dosomething

--- config/deploy/includes/international.rb
+++ config/deploy/includes/international.rb
@@ -2,7 +2,7 @@ set :deploy_to, "/var/www/international.dosomething.org"
 
 case ENV['stage']
 when production
-  role :app, %w{dosomething@dosomething@deploy.international.dosomething.org}
+  role :app, %w{dosomething@deploy.international.dosomething.org}
   server 'deploy.international.dosomething.org', user: 'dosomething', roles: %w{app}
 
 when qa
