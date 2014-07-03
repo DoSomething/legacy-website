2adc213ed85cdfe4e593e96e27f94852d35e9e91
David Furnes <dfurnes@dosomething.org>
2014-07-03 15:56:21 -0400

    Modify login test to use the modal, rather than standalone page.
    Testing through the modal since this is how the majority of users will
    log in.

--- tests/user/login.js
+++ tests/user/login.js
@@ -3,38 +3,64 @@
  * Test that a user can authenticate correctly.
  */
 
+casper.test.begin('Test that a user can authenticate correctly.', 6, function suite(test) {
+  casper.start(url, function() {
+    test.assertTitle("DoSomething.org | Largest organization for teens and social cause", "Homepage has expected title");
 
-casper.test.begin('Tests login flow', 5, function suite(test) {
-   casper.start(url, function() {
-    test.assertTitle("DoSomething.org | Largest organization for teens and social cause", "Dosomething.org homepage title is the one expected");
-    test.assertExists('#modal--login', 'login link is there');
+    // We reference the login link's ID to avoid a crazy selector.
+    test.assertExists("#link--login", "Login link exists on page for anonymous user");
+  });
 
-   });
+  casper.then(function() {
+    // Click the login link in the header navigation menu.
+    this.click("#link--login");
 
-   // Let's try a fake address first.
-   casper.thenOpen(url + '/user', function() {
-     this.echo("Try a fake email/password combo.");
-     this.fill('form[action="/user/login"]', {
-       name: 'test@example.com',
-       pass: 'zzzzz'
-     }, true);
-   });
+    // We should see a modal with the login form.
+    this.waitUntilVisible(".modal-content", function() {
+      test.assertExists(".modal-content form[action='/user/login']", "Clicking the login link shows modal login form");
+    });
+  });
 
-   casper.waitForSelector('.messages', function() {
-     test.assertExists('div.error', 'Error message appears on screen');
-     this.echo(this.fetchText('.messages'));
-   })
+  casper.then(function() {
+    // Fill and submit bogus login credentials:
+    this.fill('form[action="/user/login"]', {
+      name: 'test@example.com',
+      pass: 'zzzzz'
+    }, true);
 
-   casper.login();
+    // We should get a specific error message.
+    casper.waitForSelector('.messages', function() {
+      test.assertSelectorHasText("div.error", "unrecognized username or password", "Error message appears after submitting invalid credentials");
+    });
+  });
 
-   casper.then(function() {
-     test.assertExists('.profile--campaigns', 'Has a user profile');
-     test.assertDoesntExist('#modal--login', '"Login" link is not found.');
-   });
+  casper.thenOpen(url, function() {
+    // Now let's go back home and login using the login modal.
+    this.click("#link--login");
+    
+    // We should see a modal with the login form.
+    this.waitUntilVisible(".modal-content", function() {
+      this.fill('form[action="/user/login"]', {
+        name: 'QA_TEST_ACCOUNT@example.com',
+        pass: 'tester'
+      }, true);
+    });
 
-  casper.logout();
+    this.waitUntilVisible("#link--logout", function() {
+      test.assertExists("#link--logout", "Logout link is shown for logged in users");
+    });
+  });
+
+  casper.thenOpen(url, function() {
+    // Let's log out using the logout button.
+    this.click("#link--logout");
+    
+    this.waitUntilVisible("#link--login", function() {
+      test.assertExists("#link--login", "Login link is shown again after logging out");
+    });
+  });
 
   casper.run(function() {
-      test.done();
+    test.done();
   });
 });
