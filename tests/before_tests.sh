f80c1b5732be21b8d81b6b109d63ca5415e88a58
David Furnes <dfurnes@dosomething.org>
2014-07-03 11:17:34 -0400

    Clear localhost entries from flood table before tests.

--- tests/before_tests.sh
+++ tests/before_tests.sh
@@ -5,3 +5,8 @@
 # Tasks to run before functional testing.
 # ==============================================================================
 
+cd /var/www/vagrant/html
+
+# Clear out localhost from flood table (prevents "more than 5 failed login attempts" error)
+drush sql-query "DELETE FROM flood WHERE identifier LIKE '%127.0.0.1';"
+
