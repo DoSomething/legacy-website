a13d9cd1a315fcd1119cf1d1f69c79078589f220
David Furnes <dfurnes@dosomething.org>
2014-07-03 15:57:11 -0400

    Small tweaks to common functions.

--- tests/common.js
+++ tests/common.js
@@ -5,19 +5,26 @@
 // Define the url for all tests.
 var url = casper.cli.get('url');
 
+// Set default viewport for all tests.
+casper.options.viewportSize = { width: 1280, height: 1024 };
 
+// Use to log in before performing a test.
 casper.login = function() {
-casper.thenOpen(url + '/user', function() {
-    this.echo("Log a real user in.");
+  this.echo("Logging in as test user.");
+
+  // Go home and login.
+  casper.open(url + "/user", function() {
     this.fill('form[action="/user/login"]', {
-      name: 'test@example.com',
+      name: 'QA_TEST_ACCOUNT@example.com',
       pass: 'tester'
     }, true);
- });
+  });
 }
 
+// Use to log out after completing a test.
 casper.logout = function() {
-  casper.open(url + '/user/logout', function(){
-    this.echo('Logout');
+  // Go home and click the "Log Out" button
+  casper.open(url + "/user/logout", function() {
+    this.echo("Logging out of test user.");
   });
 }
