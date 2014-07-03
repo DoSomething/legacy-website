b870b68ad81bcd677e535a6fcdd6d3837531af79
David Furnes <dfurnes@dosomething.org>
2014-07-03 16:55:48 -0400

    Tests for pitch page and campaign signup action.

--- tests/before_tests.sh
+++ tests/before_tests.sh
@@ -10,3 +10,8 @@ cd /var/www/vagrant/html
 # Clear out localhost from flood table (prevents "more than 5 failed login attempts" error)
 drush sql-query "DELETE FROM flood WHERE identifier LIKE '%127.0.0.1';"
 
+# Get test user account UID...
+uid=$( drush user-information QA_TEST_ACCOUNT@example.com | grep "User ID" | sed -e 's/[ ]*User ID[ ]*\:[ ]*//g' )
+
+# Remove campaign signup for test account
+drush php-eval "dosomething_signup_delete_signup(1261, $uid)"
