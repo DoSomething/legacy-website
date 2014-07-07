3f11b76dddb2c06a5ed181929d64359f48a62f96
David Furnes <dfurnes@dosomething.org>
2014-07-07 19:28:37 -0400

    DRY up creating/destroying test user accounts.

--- scripts/tests/before_tests.sh
+++ scripts/tests/before_tests.sh
@@ -13,17 +13,28 @@ cd /var/www/vagrant/html
 drush sql-query "DELETE FROM flood WHERE identifier LIKE '%127.0.0.1';"
 
 
-# Delete test user account created in `user/register.js`
-echo "Creating fresh test accounts..."
-{
-  ## Delete users created during previous test runs...
-  drush user-information QA_TEST_ACCOUNT@example.com | grep "User ID" | sed -e 's/[ ]*User ID[ ]*\:[ ]*//g' | xargs -i drush user-cancel {} -y
-  drush user-information QA_TEST_USER_REGISTER@example.com | grep "User ID" | sed -e 's/[ ]*User ID[ ]*\:[ ]*//g' | xargs -i drush user-cancel {} -y
-  drush user-information QA_TEST_CAMPAIGN_SIGNUP_EXISTING@example.com | grep "User ID" | sed -e 's/[ ]*User ID[ ]*\:[ ]*//g' | xargs -i drush user-cancel {} -y
-  drush user-information QA_TEST_CAMPAIGN_SIGNUP_NEW@example.com | grep "User ID" | sed -e 's/[ ]*User ID[ ]*\:[ ]*//g' | xargs -i drush user-cancel {} -y
+# Create fresh test accounts
+drush_create_test_user() {
+  {
+    USERNAME=$1
+    drush user-create $USERNAME --mail="$USERNAME@example.com" --password="$USERNAME"
+  } &> /dev/null
+}
 
-  ## Make fresh test accounts...
-  drush user-create qa_test_account --mail=QA_TEST_ACCOUNT@example.com --password=QA_TEST_ACCOUNT
-  drush user-create qa_test_signup_existing --mail=QA_TEST_CAMPAIGN_SIGNUP_EXISTING@example.com --password=QA_TEST_CAMPAIGN_SIGNUP_EXISTING
-} &> /dev/null
+drush_delete_user_with_email() {
+  {
+    drush user-information $1 | grep "User ID" | sed -e 's/[ ]*User ID[ ]*\:[ ]*//g' | xargs -i drush user-cancel {} -y  &> /dev/null
+  } &> /dev/null
+}
+
+echo "Creating fresh test accounts..."
+## Delete users created during previous test runs...
+drush_delete_user_with_email QA_TEST_ACCOUNT@example.com
+drush_delete_user_with_email QA_TEST_USER_REGISTER@example.com
+drush_delete_user_with_email QA_TEST_CAMPAIGN_SIGNUP_EXISTING@example.com 
+drush_delete_user_with_email QA_TEST_CAMPAIGN_SIGNUP_NEW@example.com
+
+## Make fresh test accounts...
+drush_create_test_user QA_TEST_ACCOUNT
+drush_create_test_user QA_TEST_CAMPAIGN_SIGNUP_EXISTING
 
