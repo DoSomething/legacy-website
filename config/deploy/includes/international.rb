9005e9da6712da52bd6b3ee668a057f8c6915047
Morgan Rich <mrich@dosomething.org>
2014-07-07 13:45:43 -0400

    Missing quotes

--- config/deploy/includes/international.rb
+++ config/deploy/includes/international.rb
@@ -1,11 +1,11 @@
 set :deploy_to, "/var/www/international.dosomething.org"
 
 case ENV['stage']
-when production
+when 'production'
   role :app, %w{dosomething@deploy.international.dosomething.org}
   server 'deploy.international.dosomething.org', user: 'dosomething', roles: %w{app}
 
-when qa
+when 'qa'
   role :app, %w{dosomething@intl-qa}
   server 'intl-qa', user: 'dosomething', roles: %w{app}
 end
