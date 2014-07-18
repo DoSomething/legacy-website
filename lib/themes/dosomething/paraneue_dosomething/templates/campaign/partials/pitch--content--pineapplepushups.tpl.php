7230a7fd032afd3d0e90dc95d5d2db1faae722fe
Diego Lorenzo <dlorenzo@dosomething.org>
2014-07-18 13:29:43 -0400

    Adding in content within html markup structure for optimizely SEO pitch page runs.

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/partials/pitch--content--pineapplepushups.tpl.php
@@ -0,0 +1,30 @@
+<div class="container">
+  <div class="wrapper">
+
+    <div class="container__body">
+      <div class="-columned -odd">
+        <h3>The Problem</h3>
+        <p>Guys often think sexist comments aren't cool but don't speak up against them because they feel they won't be supported when they do.</p>
+
+        <h3>The Solution</h3>
+        <p>It's important for guys to shut down sexist comments, and they're more likely to do that when (a) they're supported by other guys; and (b) they have a clear and easy tactic to use.</p>
+        <p>Have at least two teammates agree to do 10 push-ups when they say something sexist. You'll call each other out with the keyword "pineapple."</p>
+      </div>
+
+      <div class="-columned -even">
+        <div class="message-box">
+          <h3 class="__title">What You Get</h3>
+          <p class="__copy">Sign up and you’ll get facts on sexism, a guide on when to call your friends out for making sexist comments, and tips on educating people why sexism sucks while staying positive.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="cta">
+  <div class="wrapper">
+    <h2 class="__message">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+    <a href="#" class="btn">Sign Up</a>
+  </div>
+</div>
