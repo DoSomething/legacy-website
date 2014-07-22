83070089459ef11b05765b69196a4f65ee7da065
David Furnes <dfurnes@dosomething.org>
2014-07-22 15:01:21 -0400

    Configure PhantomCSS.

--- scripts/tests/common.js
+++ scripts/tests/common.js
@@ -7,18 +7,35 @@ var ROOT = '/var/www/vagrant/';
 // Define the url for all tests.
 var url = casper.cli.get('url');
 var phantomcss = require(ROOT + 'scripts/tests/vendor/phantomcss');
+
+function formattedFilename(test) {
+  return test.filename.replace("/var/www/vagrant/tests/visual/", "").replace(".png", "");
+}
+
 phantomcss.init({
   libraryRoot: ROOT + '/scripts/tests/vendor',
   screenshotRoot: ROOT + '/tests/visual',
+  failedComparisonsRoot: ROOT + '/tests/visual/failures',
   addLabelToFailedImage: false,
+  mismatchTolerance: 0.01,
   onPass: function(test) {
-    casper.test.pass('No changes found for visual regression test "' + test.filename + '".');
+    casper.test.pass('No changes found for visual regression test "' + formattedFilename(test) + '".');
   },
   onFail: function(test) {
-    casper.test.fail('Visual change found in "' + test.filename + '" (' + test.mismatch + '% mismatch)');
+    casper.test.fail('Visual change found in "' + formattedFilename(test) + '" (' + test.mismatch + '% mismatch)');
   },
   onNewImage: function(test) {
-    casper.test.info('New baseline screenshot generated at "'+ test.filename + '".');
+    casper.test.info('New baseline screenshot generated at "'+ formattedFilename(test) + '".');
+  },
+  onComplete: function(tests, noOfFails, noOfErrors) {
+    if( tests.length === 0){
+      casper.test.info("No tests run.");
+    } else {
+      if(noOfFails !== 0) {
+        casper.test.info("Visual diffs generated in 'tests/visual/failures' for failing tests.");
+        casper.test.info("If changes are expected, replace testName.png with testName.fail.png to \"merge\" the changes so that future tests pass, and commit the updated test image.");
+      }
+    }
   },
   fileNameGetter: function(root,filename){ 
     var expected = root + fs.separator + filename + '.png';
@@ -29,7 +46,16 @@ phantomcss.init({
       return expected;
     }
   },
-
+  outputSettings: {
+    errorColor: {
+      red: 255,
+      green: 0,
+      blue: 255
+    },
+    errorType: 'movement',
+    transparency: 0.3,
+    largeImageThreshold: 1440
+  }
 });
 
 // Set default viewport for all tests.
