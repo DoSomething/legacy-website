affbbc54320896212cfa75bdcabf9e6612caffb1
Diego Lorenzo <dlorenzo@dosomething.org>
2014-07-31 01:24:01 -0400

    Update to allow for block output without all the block wrapper markup.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/block.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/block.tpl.php
@@ -5,15 +5,23 @@
  */
 ?>
 
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<?php if ($clean_output): ?>
 
-  <?php print render($title_prefix); ?>
-<?php if ($block->subject): ?>
-  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-<?php endif;?>
-  <?php print render($title_suffix); ?>
+  <?php print $content ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php print $content ?>
+<?php else: ?>
+
+  <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+    <?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+    <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+  <?php endif;?>
+    <?php print render($title_suffix); ?>
+
+    <div class="content"<?php print $content_attributes; ?>>
+      <?php print $content ?>
+    </div>
   </div>
-</div>
+
+<?php endif; ?>
