099fba2bbceb980548a6035db01c953b119a9327
Diego Lorenzo <dlorenzo@dosomething.org>
2014-07-31 14:58:54 -0400

    Removing markup around blocks since not needed and cleaning up preprocess function.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/block.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/block.tpl.php
@@ -2,31 +2,7 @@
 /**
  * Returns the HTML for a block.
  * @see https://drupal.org/node/1728246
- *
- * @see
- * paraneue_dosomething_preprocess_block() in includes/preprocess.inc
- * If the $clean_output variable is set to TRUE, then the content for
- * the block will be output, without any of the extra markup around it.
  */
 ?>
 
-<?php if ($clean_output): ?>
-
-  <?php print $content ?>
-
-<?php else: ?>
-
-  <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-
-    <?php print render($title_prefix); ?>
-  <?php if ($block->subject): ?>
-    <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
-  <?php endif;?>
-    <?php print render($title_suffix); ?>
-
-    <div class="content"<?php print $content_attributes; ?>>
-      <?php print $content ?>
-    </div>
-  </div>
-
-<?php endif; ?>
+<?php print $content ?>
