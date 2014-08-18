55abd25557f117f2f19ec975b39b2de25412283c
Aaron Schachter <aaronschachter@gmail.com>
2014-08-18 17:54:15 -0400

    DS-135 Adds reward-redeem-reportback-count.tpl.php

---
+++ lib/modules/dosomething/dosomething_reward/theme/reward-redeem-reportback-count.tpl.php
@@ -0,0 +1,9 @@
+<div>
+  <h1><?php print $page_header; ?></h1>
+  <p><?php print $page_copy; ?></p>
+  <div>
+    <h2><?php print $form_header; ?>
+    <p><?php print $form_copy; ?>
+    <?php print render($form); ?>
+  </div>
+</div>
