9f56b9a407de279359d6ec939a953a770a9fc11f
David Furnes <dfurnes@dosomething.org>
2014-08-26 11:15:58 -0400

    Add validation unit tests.

--- tests/unit/validation.js
+++ tests/unit/validation.js
@@ -1,164 +1,234 @@
-/* jshint ignore:start */
-require(["neue/validation", "validation/auth", "validation/reportback"], function(Validation, auth, reportback) {
-  module("Validation");
+describe("Validation", function(){
+  var Validations;
 
-  test("Name", function() {
-    Validation.Validations.name.fn("John", function(result) {
-      ok(result.message == "Hey, John!", "should greet");
-    });
-
-    Validation.Validations.name.fn("", function(result) {
-      ok(result.success == false, "should require name");
-    });
-
+  beforeEach(function() {
+    Validations = NEUE.Validation.Validations;
   });
 
-  test("Birthdays", function() {
+  describe("name", function(){
+    it("should greet the user by name", function() {
+      Validations.name.fn("John", function(result) {
+        assert.ok(result.message === "Hey, John!");
+      });
+
+      Validations.name.fn("Hou-Tai", function(result) {
+        assert.ok(result.message === "Hey, Hou-Tai!");
+      });
+
+      Validations.name.fn("明美", function(result) {
+        assert.ok(result.message === "Hey, 明美!");
+      });
+    });
+
+    it("should refuse blank names", function() {
+      Validations.name.fn("", function(result) {
+        assert.ok(result.success === false);
+      });
+    });
+  });
+
+  describe("birthday", function() {
     var now = new Date();
     var birthday_date = (now.getMonth() + 1) + "/" + now.getDate() + "/1990";
-    var user_9_years_old = Math.min(now.getMonth() + 2, 12) + "/" + now.getDate() + "/" + (now.getFullYear() - 10);
-    var user_19_years_old = Math.min(now.getMonth() + 2, 12) + "/" + now.getDate() + "/" + (now.getFullYear() - 20);
 
-    Validation.Validations.birthday.fn(birthday_date, function(result) {
-      ok(result.message == "Wow, happy birthday!", "should wish me a happy birthday");
+    it("should wish me a happy birthday", function() {
+      Validations.birthday.fn(birthday_date, function(result) {
+        assert.strictEqual(result.message, "Wow, happy birthday!");
+      });
     });
 
-    Validation.Validations.birthday.fn(user_9_years_old, function(result) {
-      ok(result.message == "Wow, you're 9!", "should tell young users their age");
+    it("should tell young users their age", function() {
+      var user_9_years_old = Math.min(now.getMonth() + 2, 12) + "/" + now.getDate() + "/" + (now.getFullYear() - 10);
+      Validations.birthday.fn(user_9_years_old, function(result) {
+        assert.strictEqual(result.message, "Wow, you're 9!");
+      });
     });
 
-    Validation.Validations.birthday.fn(user_19_years_old, function(result) {
-      ok(result.message == "Cool, 19!", "should tell me my age");
+    it("should tell target-demo users their age", function() {
+      var user_19_years_old = Math.min(now.getMonth() + 2, 12) + "/" + now.getDate() + "/" + (now.getFullYear() - 20);
+      Validations.birthday.fn(user_19_years_old, function(result) {
+        assert.strictEqual(result.message, "Cool, 19!");
+      });
     });
 
-    Validation.Validations.birthday.fn("yesterday", function(result) {
-      ok(result.success == false, "should reject non-formatted dates");
+    it("should reject non-formatted dates", function() {
+      Validations.birthday.fn("yesterday", function(result) {
+        assert.isFalse(result.success);
+      });
     });
 
-    Validation.Validations.birthday.fn("1/15/1960", function(result) {
-      ok(result.message == "Got it!", "should be cordial to old people");
+    it("should be cordial to old people", function() {
+      Validations.birthday.fn("1/15/1960", function(result) {
+        assert.strictEqual(result.message, "Got it!");
+      });
     });
 
-    Validation.Validations.birthday.fn("1/15/1860", function(result) {
-      ok(result.message == "That doesn't seem right.", "should reject users with birthdates too far in the past");
+    it("should reject users with birthdates too far in the past", function() {
+      Validations.birthday.fn("1/15/1860", function(result) {
+        assert.strictEqual(result.message, "That doesn't seem right.");
+      });
     });
 
-    Validation.Validations.birthday.fn("1/15/2095", function(result) {
-      ok(result.message == "Are you a time traveller?", "should reject users with future birthdates");
+    it("should reject users with future birthdates", function() {
+      Validations.birthday.fn("1/15/2095", function(result) {
+        assert.strictEqual(result.message, "Are you a time traveller?");
+      });
     });
 
-    Validation.Validations.birthday.fn("61/12/1990", function(result) {
-      ok(result.success == false, "should reject users with invalid month values");
+    it("should reject users with invalid month values", function() {
+      Validations.birthday.fn("61/12/1990", function(result) {
+        assert.isFalse(result.success);
+      });
     });
 
-    Validation.Validations.birthday.fn("12/42/1990", function(result) {
-      ok(result.success == false, "should reject users with day value greater than 31");
+    it("should reject users with day value greater than 31", function() {
+      Validations.birthday.fn("12/42/1990", function(result) {
+        assert.isFalse(result.success);
+      });
     });
 
-    Validation.Validations.birthday.fn("10/25/90", function(result) {
-      ok(result.success == false, "should reject dates with two-digit years");
-    });
-
-  });
-
-
-  test("Emails", function() {
-    Validation.Validations.email.fn("my.name@gmail.com", function(result) {
-      ok(result.success == true, "should accept a valid email");
-    });
-
-    Validation.Validations.email.fn("my.name.com", function(result) {
-      ok(result.success == false, "should reject an invalid email");
-    });
-
-    Validation.Validations.email.fn("myname@com", function(result) {
-      ok(result.success == false, "should reject an invalid email");
-    });
-
-    Validation.Validations.email.fn("myname@gmail.123", function(result) {
-      ok(result.success == false, "should reject an invalid email");
-    });
-  });
-
-
-  test("Passwords", function() {
-    Validation.Validations.password.fn("12345", function(result) {
-      ok(result.success == false, "should reject if too short");
-    });
-
-    Validation.Validations.password.fn("super$ecure19", function(result) {
-      ok(result.success == true, "should be totally chill with a good password");
+    it("should reject dates with two-digit years", function() {
+      Validations.birthday.fn("10/25/90", function(result) {
+        assert.isFalse(result.success);
+      });
     });
   });
 
-  test("Match", function() {
-    Validation.Validations.match.fn("dog", "cat", function(result) {
-      ok(result.success == false, "should reject if different strings");
+  describe("email", function() {
+    it("should accept a valid email", function() {
+      Validations.email.fn("my.name@gmail.com", function(result) {
+        assert.isTrue(result.success);
+      });
     });
 
-    Validation.Validations.match.fn("dog", "Dog", function(result) {
-      ok(result.success == false, "should reject if different capitalization");
-    });
+    it("should reject an invalid email", function() {
+      Validations.email.fn("my.name.com", function(result) {
+        assert.isFalse(result.success);
+      });
 
-    Validation.Validations.match.fn("dog", "dog ", function(result) {
-      ok(result.success == false, "should reject if different spacing");
-    });
+      Validations.email.fn("myname@com", function(result) {
+        assert.isFalse(result.success);
+      });
 
-    Validation.Validations.match.fn("super$ecure19", "super$ecure19", function(result) {
-      ok(result.success == true, "should accept if the same");
-    });
-  });
-
-  test("Phone numbers", function() {
-    Validation.Validations.phone.fn("123-456-7890", function(result) {
-      ok(result.success == true, "should accept 10-digit number with dashes");
-    });
-
-    Validation.Validations.phone.fn("123.456 7890", function(result) {
-      ok(result.success == true, "should accept 10-digit number with dot and space");
-    });
-
-    Validation.Validations.phone.fn("1 (123) 456-7890", function(result) {
-      ok(result.success == true, "should accept 11-digit US number");
-    });
-
-    Validation.Validations.phone.fn("123-555-9942", function(result) {
-      ok(result.success == true, "should accept number with some repeating digits");
-    });
-
-    Validation.Validations.phone.fn("1 902 #@@ 1234", function(result) {
-      ok(result.success == false, "should reject number with invalid symbols");
-    });
-
-    Validation.Validations.phone.fn("999 999 9999", function(result) {
-      ok(result.success == false, "should reject number with all repeating digits");
+      Validations.email.fn("myname@gmail.123", function(result) {
+        assert.isFalse(result.success);
+      });
     });
   });
 
-  test("Reportback numbers", function() {
-    Validation.Validations.positiveInteger.fn("19", function(result) {
-      ok(result.success == true, "should accept a valid integer");
+  describe("passwords", function() {
+    it("should be totally chill with a good password", function() {
+      Validations.password.fn("super$ecure19", function(result) {
+        assert.isTrue(result.success);
+      });
     });
 
-    Validation.Validations.positiveInteger.fn(" 5", function(result) {
-      ok(result.success == true, "should accept a number with space in front");
-    });
-
-    Validation.Validations.positiveInteger.fn("5 ", function(result) {
-      ok(result.success == true, "should accept a number with space at the end");
-    });
-
-    Validation.Validations.positiveInteger.fn("two", function(result) {
-      ok(result.success == false, "should reject a number spelled out as a string");
-    });
-
-    Validation.Validations.positiveInteger.fn("4.5", function(result) {
-      ok(result.success == false, "should reject a decimal number");
-    });
-
-    Validation.Validations.positiveInteger.fn("0", function(result) {
-      ok(result.success == false, "should reject zero");
+    it("should reject if too short", function() {
+      Validations.password.fn("12345", function(result) {
+        assert.isFalse(result.success);
+      });
     });
   });
 
+  describe("match", function() {
+    it("should reject if different strings", function() {
+      Validations.match.fn("dog", "cat", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+
+    it("should reject if different capitalization", function() {
+      Validations.match.fn("dog", "Dog", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+
+    it("should reject if different spacing", function() {
+      Validations.match.fn("dog", "dog ", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+
+    it("should accept if identical strings", function() {
+      Validations.match.fn("super$ecure19", "super$ecure19", function(result) {
+        assert.isTrue(result.success);
+      });
+    });
+  });
+
+
+  describe("phone", function() {
+    it("should accept 10-digit number dashes, dots, or spaces", function() {
+      Validations.phone.fn("123-456-7890", function(result) {
+        assert.isTrue(result.success);
+      });
+
+      Validations.phone.fn("123.456 7890", function(result) {
+        assert.isTrue(result.success);
+      });
+    });
+
+    it("should accept 11-digit US number", function() {
+      Validations.phone.fn("1 (123) 456-7890", function(result) {
+        assert.isTrue(result.success);
+      });
+    });
+
+    it("should accept number with some repeating digits", function() {
+      Validations.phone.fn("123-555-9942", function(result) {
+        assert.isTrue(result.success);
+      });
+    });
+
+    it("should reject number with invalid symbols", function() {
+      Validations.phone.fn("1 902 #@@ 1234", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+
+    it("should reject number with all repeating digits", function() {
+      Validations.phone.fn("999 999 9999", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+  });
+
+
+  describe("positiveInteger", function() {
+ 
+    it("should accept a valid integer", function() {
+      Validations.positiveInteger.fn("19", function(result) {
+        assert.isTrue(result.success);
+      });
+    });
+
+    it("should accept a number with leading or trailing space", function() {
+      Validations.positiveInteger.fn(" 5", function(result) {
+        assert.isTrue(result.success);
+      });
+
+      Validations.positiveInteger.fn("5 ", function(result) {
+        assert.isTrue(result.success);
+      });
+    });
+
+    it("should reject a number spelled out as a string", function() {
+      Validations.positiveInteger.fn("two", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+
+    it("should reject a decimal number", function() {
+      Validations.positiveInteger.fn("4.5", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+
+    it("should reject zero", function() {
+      Validations.positiveInteger.fn("0", function(result) {
+        assert.isFalse(result.success);
+      });
+    });
+  });
 });
+
