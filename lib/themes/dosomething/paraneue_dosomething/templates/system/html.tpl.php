5c4b906e250cbc88fb62a776a475ae4bac2d43c6
David Furnes <dfurnes@dosomething.org>
2014-09-11 18:18:50 -0400

    Remove unused IE-specific SCSS file.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
@@ -25,8 +25,6 @@
   <?php print $styles; ?>
 
   <!--[if lte IE 8]>
-      <link type="text/css" rel="stylesheet" href="<?php print DS_ASSET_PATH; ?>/ie.css" media="all" />
-
       <script type="text/javascript" src="<?php print VENDOR_ASSET_PATH; ?>/html5shiv/dist/html5shiv.min.js"></script>
 
       <script type="text/javascript" src="<?php print VENDOR_ASSET_PATH; ?>/respond/dest/respond.min.js"></script>
