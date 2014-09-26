2c201b71461f07c41f3dd074bcaf83c92ad0e0c9
David Furnes <dfurnes@dosomething.org>
2014-09-26 14:22:24 -0400

    Update documentation for variables.

--- tests/scripts/variables.js
+++ tests/scripts/variables.js
@@ -1,14 +1,20 @@
+/**
+ * @file
+ * Variables shared between all CasperJS tests.
+ */
+
 // Set current working directory to variable
 var ROOT = require('system').env['PWD'];
 
-// Define the url for all tests.
+// Define the URL for all tests.
 var url = casper.cli.get('url');
 
+// Reference testing campaign node on staging DB.
 var CAMPAIGN = {
   nid: 1485,
   url: url + "/node/" + 1485,
   data: require(ROOT + "/tests/fixtures/campaign.json")
 }
 
-// Set some static strings
+// Save any static strings that are used in multiple tests.
 var CAMPAIGN_SIGNUP_MESSAGE = "You're signed up for";
