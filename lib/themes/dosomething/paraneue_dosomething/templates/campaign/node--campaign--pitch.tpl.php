595ea8f94923ea88614c8b3e1684a150458785c2
Aaron Schachter <aaronschachter@gmail.com>
2014-09-26 15:31:15 -0400

    DS-337 Outputs facts, solutions, value_proposition if value_proposition is set

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign--pitch.tpl.php
@@ -5,7 +5,7 @@
  * Available Variables
  * - $campaign: A campaign object. @see dosomething_campaign_load()
  * - $classes: Additional classes passed for output (string).
- * - $scholarship: Scholarship amount (string).
+ * - $campaign_scholarship: Scholarship amount (string).
  */
 ?>
 
@@ -26,26 +26,51 @@
     </div>
   </header>
 
-  <div class="wrapper">
-
-    <div class="container container--tagline">
+  <?php if (isset($campaign->value_proposition)): ?>
+    <div class="container">
       <div class="wrapper">
-        <p class="__tagline">
-          <?php print t('A DoSomething.org Campaign. Join over 2.5 million young people taking action. Any cause, anytime, anywhere.'); ?>
-          <em><?php print t('*Mic drop'); ?></em>
-        </p>
-      </div>
-    </div>
+        <div class="container__body">
+          <div class="-columned -odd">
+            <h3><?php print t('The Problem'); ?></h3>
+            <p><?php print $campaign->fact_problem['fact']; ?></p>
 
-  </div>
+            <h3><?php print t('The Solution'); ?></h3>
+            <?php // @TODO: DRY this logic with action page via $campaign ?>
+            <?php if (isset($campaign->fact_solution)): ?>
+              <p><?php print $campaign->fact_solution['fact']; ?></p>
+            <?php elseif (isset($campaign->solution_copy)): ?>
+              <?php print $campaign->solution_copy; ?>
+            <?php endif; ?>
+
+            <?php if (isset($campaign->solution_support)): ?>
+              <?php print $campaign->solution_support; ?>
+            <?php endif; ?>
+          </div>
+          <div class="-columned -even">
+            <h3 class="__title"><?php print t('What You Get'); ?></h3>
+            <p class="__copy"><?php print $campaign->value_proposition; ?></p>
+          </div>
+        </div>
+      </div><!-- .wrapper -->
+    </div><!-- .container -->
+  <?php endif; ?>
 
   <?php if (isset($campaign->secondary_call_to_action)): ?>
-  <div class="cta">
+    <div class="cta">
+      <div class="wrapper">
+        <h2 class="__message"><?php print $campaign->secondary_call_to_action; ?></h2>
+        <?php print render($signup_button); ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <div class="container container--tagline">
     <div class="wrapper">
-      <h2 class="__message"><?php print $campaign->secondary_call_to_action; ?></h2>
-      <?php print render($signup_button); ?>
+      <p class="__tagline">
+        <?php print t('A DoSomething.org Campaign. Join over 2.5 million young people taking action. Any cause, anytime, anywhere.'); ?>
+        <em><?php print t('*Mic drop'); ?></em>
+      </p>
     </div>
   </div>
-  <?php endif; ?>
 
 </section>
