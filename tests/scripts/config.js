3c300872bf8828bb4e906b9a249fd3d85e8a90ef
David Furnes <dfurnes@dosomething.org>
2014-09-26 14:19:22 -0400

    Update documentation for CasperJS helper methods.

--- tests/scripts/config.js
+++ tests/scripts/config.js
@@ -4,9 +4,6 @@ var ROOT = require('system').env['PWD'];
 // Define the url for all tests.
 var url = casper.cli.get('url');
 
-// Set default viewport for all tests.
-casper.options.viewportSize = { width: 1280, height: 1024 };
-
 var CAMPAIGN = {
   nid: 1485,
   url: url + "/node/" + 1485,
