6708d8ea0cb24020009ab12cae0560d664a42274
Shae Smith <sbsmith86@gmail.com>
2014-11-04 16:49:02 -0500

    Adding media pattern template file

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/media.tpl.php
@@ -0,0 +1,16 @@
+<article class="media">
+  <div class="wrapper">
+    <img alt="kitten overlords" src="http://placekitten.com/g/300/300" />
+  </div>
+
+  <div class="__body">
+    <h3 class="__title"><a href=
+        "http://twitter.com/harryshumjr">@HarryShumJr</a></h3>
+
+    <div class="__description">
+      Do you have stained or ripped clothes you don't wear? Recycle them
+      w/ <a href="http://twitter.com/dosomething">@DoSomething</a>
+      <a href="http://twitter.com/hmusa">http://dsorg.us/CLOTHES</a>
+    </div>
+  </div>
+</article>
