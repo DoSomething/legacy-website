ee6138361bf66b5a6bd49b7efd453a7bfc82a527
Aaron Schachter <aaronschachter@gmail.com>
2014-11-07 17:05:36 -0500

    Adds triad tpl

--- lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/gallery-triad.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/gallery-triad.tpl.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Generates the HTML for the Neue Gallery Duo pattern.
+ * Generates the HTML for the Neue Gallery Triad pattern.
  *
  * Available variables:
  * - $items: An array of themed items or tiles to put in the gallery.
@@ -9,10 +9,11 @@
  * @see paraneue_get_gallery()
  */
 ?>
-<ul class="gallery -duo">
+<ul class="gallery -triad">
   <?php if (!empty($items)): ?>
     <?php foreach ($items as $delta => $item): ?>
-      <li class="<?php ($delta % 2) ? print '-second' : print '-first' ?>">
+      <?php // @todo: This is kinda nasty. Preprocess it? ?>
+      <li class="<?php print paraneue_dosomething_get_gallery_item_order_class($delta); ?>">
         <?php print $item; ?>
       </li>
     <?php endforeach; ?>
