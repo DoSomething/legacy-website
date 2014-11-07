fec9aee982ced6146fe2005050eaa7b655a8531e
Aaron Schachter <aaronschachter@gmail.com>
2014-11-07 17:59:20 -0500

    Fixes #3299

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/figure.tpl.php
@@ -0,0 +1,17 @@
+<div class="tile tile--figure">
+  <?php if (!empty($content['image'])): ?>
+    <div class="__media">
+      <?php print $content['image']; ?>
+    </div>
+  <?php endif; ?>
+  <?php if (!empty($content['title'])): ?>
+  <h3 class="__title">
+    <?php print $content['title']; ?>
+  </h3>
+  <?php endif; ?>
+  <?php if (!empty($content['description'])): ?>
+  <div class="__description">
+    <?php print $content['description']; ?>
+  </div>
+  <?php endif; ?>
+</div>
