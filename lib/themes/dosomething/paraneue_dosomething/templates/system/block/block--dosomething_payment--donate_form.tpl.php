e642078528f86ea6239f02c3e776ecbf83159ce0
Tom Rosario <hello@tomrosario.com>
2014-11-13 13:57:21 -0500

    updated form donate form fields on payment module

--- lib/themes/dosomething/paraneue_dosomething/templates/system/block/block--dosomething_payment--donate_form.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/block/block--dosomething_payment--donate_form.tpl.php
@@ -11,7 +11,7 @@
     <a href="#" data-modal-href="#modal--donate-form" class="btn">
       <?php print t("Donate"); ?>
     </a>
-    <div data-modal id="modal--donate-form" role="dialog">
+    <div data-modal id="modal--donate-form" role="dialog" class="donate--payment">
       <h2 class="donate--header"><?php print $block->subject; ?></h2>
       <?php print $content; ?>
     </div>
