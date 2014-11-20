04d12d6417a3eca306e4acc9d202008ebda183af
Joe Kent <thedeadlybutter@gmail.com>
2014-11-20 17:00:53 -0500

    Fixed location

--- lib/themes/dosomething/paraneue_dosomething/templates/taxonomy/taxonomy-term.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/taxonomy/taxonomy-term.tpl.php
@@ -41,16 +41,6 @@
     </section>
   <?php endif; ?>
 
-  
-  <div class="cta">
-    <div class="wrapper">
-      <?php if (isset($call_to_action)): ?>
-        <h2 class="__message"><?php print $call_to_action; ?></h2>
-      <?php endif; ?>
-      <a href="#gallery" class="btn"><?php print t("Do it"); ?></a>
-    </div>
-  </div>
-
 
   <?php if (!empty($campaign_gallery)): ?>
     <section id="gallery" class="container container--campaigns">
@@ -103,6 +93,15 @@
 
     </div>
   </section>
+
+  <div class="cta">
+    <div class="wrapper">
+      <?php if (isset($call_to_action)): ?>
+        <h2 class="__message"><?php print $call_to_action; ?></h2>
+      <?php endif; ?>
+      <a href="#gallery" class="btn"><?php print t("Do it"); ?></a>
+    </div>
+  </div>
 
   <?php if ($info_bar): ?>
     <?php print $info_bar; ?>
