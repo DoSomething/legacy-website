21fec5b02bed9e9fbfb95a27cd2c9a60f83220b7
DSMACAIR2 <dosomething@DSMACAIR2s-MacBook-Air.local>
2014-12-02 17:18:59 -0500

    linking image to content

--- lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/figure.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/figure.tpl.php
@@ -1,7 +1,13 @@
 <div class="tile tile--figure">
   <?php if (!empty($content['image'])): ?>
     <div class="__media">
-      <?php print $content['image']; ?>
+      <?php if (!empty($content['url'])): ?>
+        <a href="<?php print $content['url']; ?>">
+          <?php print $content['image']; ?>
+        </a>
+      <?php else: ?>
+        <?php print $content['image']; ?>
+      <?php endif; ?>
     </div>
   <?php endif; ?>
   <?php if (!empty($content['title'])): ?>
