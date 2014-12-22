9efb6a35cb304b94d414b0de33442d81e4cfc227
Shae Smith <sbsmith86@gmail.com>
2014-12-22 14:35:52 -0500

    Image crop js populate form with crop values

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign/node--campaign.tpl.php
@@ -366,7 +366,6 @@
                   <!-- This is a purely frontend form that will grab crop options and a caption
                   And then when the user submits the form, it will populate the drupal form with these values. -->
                   <form action="#" method="post" id="dosomething-reportback-image-form" accept-charset="UTF-8">
-                    <!-- Hidden form elements to go here -->
                     <label for="caption">Caption</label>
                     <input placeholder="Write something..." type="text" id="caption" name="caption" value="" size="60" maxlength="128">
                     <input type="submit" value="done" class="btn secondary" />
