91e5fbfef177a24ff17a0732d3408c660a2d4419
David Furnes <david@dfurnes.com>
2015-01-08 10:43:46 -0500

    Update navigation markup for Neue 6.0.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/partials/navigation.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/partials/navigation.tpl.php
@@ -3,32 +3,32 @@
 /**
  * Generates site-wide global page nav.
  **/
+
 ?>
 
-<nav class="chrome--nav<?php if(isset($modifier_classes)): print ' ' . $modifier_classes; endif; ?>">
-  <a class="logo" href="<?php print $base_path; ?>"><img src="<?php print $logo ?>" ></a>
-  <a class="hamburger js-toggle-mobile-menu" href="#">&#xe606;</a>
-  <div class="menu">
-    <ul class="primary-nav">
+<nav class="navigation<?php if(isset($modifier_classes)): print ' ' . $modifier_classes; endif; ?>">
+  <a class="navigation__logo" href="<?php print $base_path; ?>"><span>DoSomething.org</span></a>
+  <a class="navigation__toggle js-navigation-toggle" href="#"><span>Show Menu</span></a>
+  <div class="navigation__menu">
+    <ul class="navigation__primary">
       <li>
         <a href="/campaigns">
-          <strong><?php print $explore_campaigns['text']; ?></strong>
-          <span><?php print $explore_campaigns['subtext']; ?></span>
+          <strong class="navigation__title"><?php print $explore_campaigns['text']; ?></strong>
+          <span class="navigation__subtitle"><?php print $explore_campaigns['subtext']; ?></span>
         </a>
       </li>
 
       <li>
         <a href="<?php print url('node/' . $who_we_are['link']); ?>">
-          <?php //<strong>What is D<span>o</span>S<span>omething</span>.org?</strong> @TODO: Temp hidden. ?>
-          <strong><?php print $who_we_are['text']; ?></strong>
-          <span><?php print $who_we_are['subtext']; ?></span>
+          <strong class="navigation__title"><?php print $who_we_are['text']; ?></strong>
+          <span class="navigation__subtitle"><?php print $who_we_are['subtext']; ?></span>
         </a>
       </li>
     </ul>
 
-    <div class="secondary-nav">
+    <div class="navigation__secondary">
       <ul>
-        <li class="searchfield">
+        <li>
           <?php print $search_box; ?>
         </li>
         <?php if(!$logged_in): ?>
@@ -39,9 +39,9 @@
         <li class="login"><a id="link--login" class="secondary-nav-item" href="<?php print $front_page; ?>user/login" data-modal-href="#modal--login"><?php print t('Log In'); ?></a></li>
         <?php else: ?>
         <?php if( theme_get_setting('show_profile_link') ): ?>
-        <li class="account"><?php print l($user_identifier, 'user/'. $user->uid); ?></li>
+        <li><?php print l($user_identifier, 'user/'. $user->uid); ?></li>
         <?php endif; ?>
-        <li class="login"><a id="link--logout" href="<?php print $front_page; ?>user/logout" class="secondary-nav-item"><?php print t('Log Out'); ?></a></li>
+        <li><a id="link--logout" href="<?php print $front_page; ?>user/logout"><?php print t('Log Out'); ?></a></li>
         <?php endif; ?>
       </ul>
     </div>
