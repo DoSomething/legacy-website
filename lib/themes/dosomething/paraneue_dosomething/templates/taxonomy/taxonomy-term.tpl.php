bdc79ec13487ff61502fae56bf47cdad75c3590e
David Furnes <david@dfurnes.com>
2015-01-12 15:12:11 -0500

    Update taxonomy page markup for Neue 6.0.

--- lib/themes/dosomething/paraneue_dosomething/templates/taxonomy/taxonomy-term.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/taxonomy/taxonomy-term.tpl.php
@@ -16,27 +16,25 @@
     <section class="container container--intro">
       <div class="wrapper">
         <?php if (isset($intro_title)): ?>
-          <h2 class="container__title inline--alt-color"><?php print $intro_title; ?></h2>
+          <h2 class="inline--alt-color"><?php print $intro_title; ?></h2>
         <?php endif; ?>
-        <div class="container__body">
-          <div<?php if (isset($intro_image) || isset($intro_video)): print ' class="-columned"'; endif; ?>>
-            <?php print $intro; ?>
-            <?php if (isset($modals)): ?>
-              <?php print $modals; ?>
-            <?php endif; ?>
-          </div>
-          <?php if (isset($intro_image) || isset($intro_video)): ?>
-          <aside class="-columned -col-last">
-            <?php if (isset($intro_video)): ?>
-              <div class="media-video">
-                <?php print $intro_video; ?>
-              </div>
-            <?php elseif (isset($intro_image)): ?>
-              <?php print $intro_image; ?>
-            <?php endif; ?>
-          </aside>
+        <div class="wrapper <?php if (isset($intro_image) || isset($intro_video)): print '-half'; else: print '-narrow'; endif; ?>">
+          <?php print $intro; ?>
+          <?php if (isset($modals)): ?>
+            <?php print $modals; ?>
           <?php endif; ?>
         </div>
+        <?php if (isset($intro_image) || isset($intro_video)): ?>
+        <div class="wrapper -half">
+          <?php if (isset($intro_video)): ?>
+            <div class="media-video">
+              <?php print $intro_video; ?>
+            </div>
+          <?php elseif (isset($intro_image)): ?>
+            <?php print $intro_image; ?>
+          <?php endif; ?>
+        </div>
+        <?php endif; ?>
       </div>
     </section>
   <?php endif; ?>
@@ -49,17 +47,15 @@
   <?php endif; ?>
 
   <section class="container additional-text">
-    <div class="wrapper">
-
+    <div class="wrapper -narrow">
       <?php if (!empty($facts)): ?>
-        <h2 class="container__title inline--alt-color">
-          <?php print $facts_header; ?>
-        </h2>
+        <h2 class="inline--alt-color"><?php print $facts_header; ?></h2>
         <ul>
-        <?php foreach ($facts['facts'] as $fact): ?>
-          <li><?php print $fact['fact']; ?> <sup><?php print $fact['footnotes']; ?></sup></li>
-        <?php endforeach; ?>
+          <?php foreach ($facts['facts'] as $fact): ?>
+            <li><?php print $fact['fact']; ?> <sup><?php print $fact['footnotes']; ?></sup></li>
+          <?php endforeach; ?>
         </ul>
+
         <section class="sources">
           <h3 class="__title js-toggle-sources"><?php print t('Sources'); ?></h3>
           <ul class="__body legal">
@@ -71,26 +67,19 @@
       <?php endif; ?>
 
       <?php if (isset($global_copy['campaign_value_proposition'])): ?>
-        <h2 class="container__title inline--alt-color">
-          <?php print t('What You Get'); ?>
-        </h2>
+        <h2 class="inline--alt-color"><?php print t('What You Get'); ?></h2>
         <?php print $global_copy['campaign_value_proposition']; ?>
       <?php endif; ?>
 
       <?php if (isset($global_copy['scholarships'])): ?>
-        <h2 class="container__title inline--alt-color">
-          <?php print t('Scholarships and Contests'); ?>
-        </h2>
+        <h2 class="inline--alt-color"><?php print t('Scholarships and Contests'); ?></h2>
         <?php print $global_copy['scholarships']; ?>
       <?php endif; ?>
 
       <?php if (isset($global_copy['about_ds'])): ?>
-        <h2 class="container__title inline--alt-color">
-          <?php print t('About DoSomething.org'); ?>
-        </h2>
+        <h2 class="inline--alt-color"><?php print t('About DoSomething.org'); ?></h2>
         <?php print $global_copy['about_ds']; ?>
       <?php endif; ?>
-
     </div>
   </section>
 
