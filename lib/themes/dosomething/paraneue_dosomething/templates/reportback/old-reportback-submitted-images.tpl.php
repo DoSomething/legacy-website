f7f985d34b771f98671f3b6fd81217ab4fd022c3
Diego Lorenzo <dlorenzo@dosomething.org>
2015-01-27 14:58:40 -0500

    Adding proper fixes to account for feature flags for Production.

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/old-reportback-submitted-images.tpl.php
@@ -0,0 +1,6 @@
+<div class="submitted-image">
+  <p class="legal"><?php print t("Last updated: @updated", array("@updated" => $updated)); ?></p>
+  <?php foreach ($images as $image): ?>
+    <?php print $image; ?>
+  <?php endforeach; ?>
+</div>
