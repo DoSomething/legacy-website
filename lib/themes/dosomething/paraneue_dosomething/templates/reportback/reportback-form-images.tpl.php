c6618043a5565bdb031fc0681dbdd8042c85fc21
Diego Lorenzo <dlorenzo@dosomething.org>
2015-01-27 12:30:08 -0500

    Wrapping up markup and layout for form functionality and some basic styles.

--- lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-form-images.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($images as $image): ?>
-  <?php print $image; ?>
-<?php endforeach; ?>
---
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-latest-submission.tpl.php
@@ -0,0 +1,5 @@
+<figure class="reportback__submission-latest">
+  <?php print $image; ?>
+
+  <figcaption>Need to pull caption in from somewhere for the image.</figcaption>
+</figure>
