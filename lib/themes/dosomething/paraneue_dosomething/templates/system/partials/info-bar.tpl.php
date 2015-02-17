c136c74c82a00d350b593431b841e24ce923c90d
David Furnes <david@dfurnes.com>
2015-02-17 14:19:11 -0500

    Swap positions of "contact us" and partner text in Info Bar.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/partials/info-bar.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/partials/info-bar.tpl.php
@@ -8,23 +8,22 @@
 <?php if (isset($zendesk_form) || isset($formatted_partners)): ?>
 <footer class="info-bar">
   <div class="wrapper">
-
-    <?php if (isset($zendesk_form)): ?>
-      <?php print t('Questions?'); ?> <a href="#" data-modal-href="#modal-contact-form"><?php print t('Contact Us'); ?></a>
-      <div data-modal id="modal-contact-form" class="modal--contact" role="dialog">
-        <h2 class="heading -banner"><?php print t('Contact Us'); ?></h2>
-        <div class="modal__block">
-          <p><?php print $zendesk_form_header; ?></p>
-        </div>
-        <div class="modal__block">
-          <?php print render($zendesk_form); ?>
-        </div>
-      </div>
+    <?php if (isset($formatted_partners)): ?>
+      <?php print t("In partnership with"); ?> <?php print $formatted_partners; ?>
     <?php endif; ?>
 
-    <?php if (isset($formatted_partners)): ?>
+    <?php if (isset($zendesk_form)): ?>
       <div class="info-bar__secondary">
-      <?php print t("In partnership with"); ?> <?php print $formatted_partners; ?>
+        <?php print t('Questions?'); ?> <a href="#" data-modal-href="#modal-contact-form"><?php print t('Contact Us'); ?></a>
+        <div data-modal id="modal-contact-form" class="modal--contact" role="dialog">
+          <h2 class="heading -banner"><?php print t('Contact Us'); ?></h2>
+          <div class="modal__block">
+            <p><?php print $zendesk_form_header; ?></p>
+          </div>
+          <div class="modal__block">
+            <?php print render($zendesk_form); ?>
+          </div>
+        </div>
       </div>
     <?php endif; ?>
   </div>
