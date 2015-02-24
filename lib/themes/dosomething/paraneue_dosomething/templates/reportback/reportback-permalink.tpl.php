c03aaa950417aef386d523d126c6273d98e8b9f5
Andrea Gaither <angaither@gmail.com>
2015-02-24 11:34:45 -0500

    Print some vars on that reportback page. - campaign title - cta - problem - solution - rb image - rb caption

--- lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
@@ -1,1 +1,19 @@
-<?php var_dump($entity) ?>
+
+<h2> <?php echo $node->title ?> </h2>
+<?php echo $node->call_to_action ?>
+
+<h3> The Problem </h3>
+<?php echo $node->fact_problem['fact'] ?>
+
+<h3> The Solution </h3>
+<?php echo $node->fact_solution['fact'] ?>
+
+<?php echo $rb_image ?>
+
+<?php echo $reportback->caption ?>
+
+<?php echo $reportback->why_participated ?>
+
+<?php echo $reportback->quantity ?> <?php echo $reportback->quantity_label ?>
+
+<?php echo $reportback->participates ?>
