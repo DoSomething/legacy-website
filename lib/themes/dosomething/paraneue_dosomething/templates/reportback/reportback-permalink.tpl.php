48a8ec656b02336f0f197bdf4fbc5f24650dabf4
Andrea Gaither <angaither@gmail.com>
2015-02-24 16:45:08 -0500

    Hide and display things based on current user.

--- lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
@@ -1,20 +1,29 @@
 
 <h2> <?php echo $node->title ?> </h2>
-<?php echo $node->call_to_action ?>
 
-<h3> The Problem </h3>
-<?php echo $node->fact_problem['fact'] ?>
+<?php if (!$is_current): ?>
+  <?php echo $node->call_to_action ?>
 
-<h3> The Solution </h3>
-<?php echo $node->fact_solution['fact'] ?>
+  <h3> The Problem </h3>
+  <?php echo $node->fact_problem['fact'] ?>
+
+  <h3> The Solution </h3>
+  <?php echo $node->fact_solution['fact'] ?>
+
+<?php endif ?>
 
 <?php echo $rb_image ?>
 
-<?php echo $reportback->caption ?>
-<?php echo $user->first_name ?>
+<?php // Used to determine if the curernt logged in user is the user who submitted the rb. ?>
+<?php if ($is_current): ?>
 
-<?php echo $reportback->why_participated ?>
+  <?php echo $reportback->caption ?>
+  <?php echo $user->first_name ?>
 
-<?php echo $reportback->quantity ?> <?php echo $reportback->quantity_label ?>
+  <h3> In your words </h3>
+  <?php echo $reportback->why_participated ?>
 
-<?php echo $reportback->participates ?>
+  <?php echo $reportback->quantity ?> <?php echo $reportback->quantity_label ?>
+
+  <?php echo $reportback->participates ?>
+<?php endif ?>
