147b3f2349cde7fc5e3ef5559b394d9bcfa752df
Shae Smith <ssmith@dosomething.org>
2015-03-04 18:09:38 -0500

    Updating padding

--- lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-permalink.tpl.php
@@ -4,27 +4,23 @@
  * Reportback confirmation/permalink page.
  *
  * Available variables:
+ * - $rb_image    : Reportback image.
  * - $node->title : Title of the campaign
  * - $is_current  : Used to determine if the curernt logged in user is the user who submitted the rb.
+ * - $copy_vars['owners_rb_subtitle']    : Title of the reportback confirmation page
+ * - $copy_vars['owners_rb_scholarship'] : Subtitle of the reportback confirmation page.
+ * - $copy_vars['owners_rb_important']   : Title of the why I participated section
+ * - $reportback->why_participated       : Why I participated copy of the reportback
+ * - $reportback->quantity               : Reportback quanitity
+ * - $reportback->quantity_label         : Quantity lable
  * - $node->fact_problem['fact']
  * - $node->fact_solution['fact']
- * - $rb_image
- * - $copy_vars['owners_rb_subtitle']
- * - $copy_vars['owners_rb_scholarship']
  * - $reportback->caption
  * - $user->first_name
- * - $copy_vars['owners_rb_important']
- * - $reportback->why_participated
- * - $reportback->quantity
- * - $reportback->quantity_label
  *
- * @see template_preprocess()
- * @see template_preprocess_block()
- * @see template_process()
- *
- * @ingroup themeable
  */
 ?>
+
 <article class="reportback__permalink">
   <header role="banner" class="header ">
     <div class="wrapper">
@@ -36,15 +32,14 @@
   <div class="container -padded -purple">
     <div class="wrapper">
       <div class="container -white">
-        <div class="container__block -half">
-          <div><?php print $rb_image ?></div>
-          <!-- <div class="caption"><?php print check_plain($reportback->caption) ?></div> -->
-          <div class="caption">Squad up to stomp down bullying - Leah</div>
+        <div class="container__block -half -border">
+          <?php print $rb_image ?>
+          <div class="caption"><?php print check_plain($reportback->caption) ?></div>
         </div>
         <div class="container__block -half">
           <h1><?php print $node->title ?></h1>
           <p class="heading -gamma"><?php print check_plain($reportback->quantity) ?> <?php print $reportback->quantity_label ?></p>
-          <h2><?php print $copy_vars['owners_rb_important'] ?></h2>
+          <h3><?php print $copy_vars['owners_rb_important'] ?></h3>
           <p><?php print check_plain($reportback->why_participated) ?></p>
         </div>
       </div>
