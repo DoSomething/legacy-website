70336db9b0966e8a760092cbb6671438794f3890
Aaron Schachter <aaronschachter@gmail.com>
2015-03-19 14:17:39 -0700

    Adds messy faq and tags

--- lib/modules/dosomething/dosomething_api/plugins/restful/campaigns/1.0/DoSomethingCampaignsResource.class.php
+++ lib/modules/dosomething/dosomething_api/plugins/restful/campaigns/1.0/DoSomethingCampaignsResource.class.php
@@ -15,7 +15,12 @@ class DoSomethingCampaignsResource extends RestfulEntityBaseNode {
     $public_fields['call_to_action'] = array(
       'property' => 'field_call_to_action',
     );
-
+    $public_fields['faq'] = array(
+      'property' => 'field_faq',
+    );
+    $public_fields['tags'] = array(
+      'property' => 'field_tags',
+    );
     return $public_fields;
   }
 }
