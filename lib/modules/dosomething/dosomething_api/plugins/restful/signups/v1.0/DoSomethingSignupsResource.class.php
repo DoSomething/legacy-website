6ff95ecc1281201dca30fcacf90bbaaf374d92fc
Aaron Schachter <aaronschachter@gmail.com>
2015-03-19 17:12:32 -0700

    Gets Signup resource references working, adds dosomething_signup_entity_property_info

--- lib/modules/dosomething/dosomething_api/plugins/restful/signups/v1.0/DoSomethingSignupsResource.class.php
+++ lib/modules/dosomething/dosomething_api/plugins/restful/signups/v1.0/DoSomethingSignupsResource.class.php
@@ -16,23 +16,24 @@ class DoSomethingSignupsResource extends RestfulEntityBase {
       'property' => 'timestamp',
     );
 
-    // $public_fields['campaign'] = array(
-    //   'property' => 'nid',
-    //    'resource' => array(
-    //       'campaign' => 'campaigns',
-    //     ),
-    // );
+    $public_fields['campaign'] = array(
+      'property' => 'nid',
+       'resource' => array(
+          'campaign' => 'campaigns',
+        ),
+    );
 
-    // $public_fields['user'] = array(
-    //   'property' => 'uid',
-    //    'resource' => array(
-    //       'user' => 'users',
-    //     ),
-    // );
+    $public_fields['user'] = array(
+      'property' => 'uid',
+      'resource' => array(
+        'user' => 'users',
+      ),
+    );
 
-    // $public_fields['source'] = array(
-    //   'property' => 'source',
-    // );
+
+    $public_fields['source'] = array(
+      'property' => 'source',
+    );
 
     return $public_fields;
   }
