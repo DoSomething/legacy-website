019dace6f5f35ee03bc33b9d90fb10883e8f930f
Diego Lorenzo <dlorenzo@dosomething.org>
2015-03-23 12:32:36 -0400

    Update to enabled admins to click on button on Reportbacks to edit their status.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/photo.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/photo.tpl.php
@@ -1,4 +1,9 @@
 <figure class="photo -stacked -framed">
+  <?php if (user_access('view any reportback')): ?>
+    <div class="admin-edit">
+      <a class="button -secondary" href="/admin/reportback/<?php print $content['rbid'] ?>">Edit Status</a>
+    </div>
+  <?php endif; ?>
   <img src="<?php print $content['image']; ?>" alt="<?php print check_plain($content['caption']); ?>" />
   <figcaption class="__copy">
     <?php print check_plain($content['caption']); ?>
