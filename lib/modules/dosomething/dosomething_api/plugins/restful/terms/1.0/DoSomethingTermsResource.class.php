21869fdc2b804f56bbe8f33bbbe0bb5bd06bf66c
andrew morton <drewish@katherinehouse.com>
2015-04-02 21:55:45 -0600

    Expose vocabulary_id to allow filtering of terms

--- lib/modules/dosomething/dosomething_api/plugins/restful/terms/1.0/DoSomethingTermsResource.class.php
+++ lib/modules/dosomething/dosomething_api/plugins/restful/terms/1.0/DoSomethingTermsResource.class.php
@@ -10,4 +10,14 @@ class DoSomethingTermsResource extends \RestfulEntityBaseTaxonomyTerm {
   /**
    * Overrides \RestfulEntityBaseTaxonomyTerm::publicFieldsInfo().
    */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['vocabulary_id'] = array(
+      'property' => 'vocabulary',
+    );
+
+    return $public_fields;
+  }
+
 }
