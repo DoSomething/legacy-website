83cc2f62a144e0bbd7972ab28db4959e68b87d34
David Furnes <david@dfurnes.com>
2015-05-13 12:04:16 -0400

    Fix action guide markup.

--- lib/modules/dosomething/dosomething_action_guide/node--action_guide.tpl.php
+++ lib/modules/dosomething/dosomething_action_guide/node--action_guide.tpl.php
@@ -2,40 +2,50 @@
 
   <h2 class="heading -banner"><?php print $title; ?></h2>
 
-  <?php if (isset($subtitle)): ?>
-    <h3 class="subtitle"><?php print $subtitle; ?></h3>
+  <div class="modal__block">
+    <?php if (isset($subtitle)): ?>
+      <h3 class="subtitle"><?php print $subtitle; ?></h3>
+    <?php endif; ?>
+
+    <?php if (isset($intro_title)): ?>
+      <h2><?php print $intro_title; ?></h2>
+    <?php endif; ?>
+
+    <?php if (isset($intro)): ?>
+      <div><?php print $intro['safe_value']; ?></div>
+    <?php endif; ?>
+  </div>
+
+  <?php if (!empty($intro_image)): ?>
+    <div class="modal__block">
+      <?php print $intro_image; ?>
+    </div>
   <?php endif; ?>
 
-  <?php if (isset($intro_title)): ?>
-    <h2><?php print $intro_title; ?></h2>
+  <?php if (!empty($galleries)): ?>
+    <div class="modal__block">
+      <?php foreach ($galleries as $gallery): ?>
+        <?php print $gallery['image']; ?>
+        <?php if ($gallery['image_title']): ?>
+          <h3><?php print $gallery['image_title']; ?></h3>
+        <?php endif; ?>
+        <?php if ($gallery['image_description']): ?>
+          <p><?php print $gallery['image_description']; ?></p>
+        <?php endif; ?>
+      <?php endforeach; ?>
+    </div>
   <?php endif; ?>
 
-  <?php if (isset($intro)): ?>
-    <div><?php print $intro['safe_value']; ?></div>
-  <?php endif; ?>
-
-  <?php if (isset($intro_image)): ?>
-    <?php print $intro_image; ?>
-  <?php endif; ?>
-
-  <?php if (isset($galleries)): ?>
-    <?php foreach ($galleries as $gallery): ?>
-      <?php print $gallery['image']; ?>
-      <?php if ($gallery['image_title']): ?>
-        <h3><?php print $gallery['image_title']; ?></h3>
+  <?php if (isset($additional_text_title) || isset($additional_text)): ?>
+    <div class="modal__block">
+      <?php if (isset($additional_text_title)): ?>
+        <h2><?php print $additional_text_title; ?></h2>
       <?php endif; ?>
-      <?php if ($gallery['image_description']): ?>
-        <p><?php print $gallery['image_description']; ?></p>
+
+      <?php if (isset($additional_text)): ?>
+        <?php print $additional_text['safe_value']; ?>
       <?php endif; ?>
-    <?php endforeach; ?>
-  <?php endif; ?>
-
-  <?php if (isset($additional_text_title)): ?>
-    <h2><?php print $additional_text_title; ?></h2>
-  <?php endif; ?>
-
-  <?php if (isset($additional_text)): ?>
-    <?php print $additional_text['safe_value']; ?>
+    </div>
   <?php endif; ?>
 
 </article>
