2115cf977f8be8e34b8b53e9dfc626c9e655da50
Diego Lorenzo <dlorenzo@dosomething.org>
2015-05-14 18:18:28 -0400

    Added methods for getting facts, solutions, cause and issue.

--- lib/modules/dosomething/dosomething_campaign/includes/Campaign.php
+++ lib/modules/dosomething/dosomething_campaign/includes/Campaign.php
@@ -22,6 +22,18 @@ class Campaign {
       $this->cover_image = $this->getCoverImage();
       $this->cover_image_alt = $this->getCoverImageAlt();
       $this->scholarship = $this->getScholarship();
+      $this->is_staff_pick = $this->getStaffPickStatus();
+
+      $fact_data = $this->getFactData();
+      $this->fact_problem = $fact_data['fact_problem'];
+      $this->fact_solution = $fact_data['fact_solution'];
+      $this->fact_sources = $fact_data['sources'];
+
+      $solution_data = $this->getSolutionData();
+      $this->solution = $solution_data;
+
+      $this->primary_cause = $this->getPrimaryCause();
+      $this->issue = $this->getIssue();
     }
     else {
       throw new Exception('Campaign does not exist!');
@@ -89,12 +101,68 @@ class Campaign {
     }
   }
 
+
+  protected function getFactData() {
+    $data = array();
+
+    $fact_fields = array('field_fact_problem', 'field_fact_solution');
+    $fact_vars = dosomething_fact_get_mutiple_fact_field_vars($this->node, $fact_fields);
+    if (!empty($fact_vars)) {
+      $data['fact_problem'] = $fact_vars['facts']['field_fact_problem'] ?: NULL;
+      $data['fact_solution'] = $fact_vars['facts']['field_fact_solution'] ?: NULL;
+      $data['sources'] = $fact_vars['sources'] ?: NULL;
+    }
+
+    return $data;
+  }
+
+
+  protected function getIssue() {
+    $data = array();
+
+    $issue_id = $this->extractValue($this->node->field_issue);
+    $issue = taxonomy_term_load($issue_id);
+
+    $data['tid'] = $issue->tid;
+    $data['name'] = strtolower($issue->name);
+
+    return $data;
+  }
+
+
+  protected function getPrimaryCause() {
+    $data = array();
+
+    $cause_id = $this->extractValue($this->node->field_primary_cause);
+    $cause = taxonomy_term_load($cause_id);
+
+    $data['tid'] = $cause->tid;
+    $data['name'] = strtolower($cause->name);
+
+    return $data;
+  }
+
+
   protected function getTagline() {
     return $this->extractValue($this->node->field_call_to_action);
   }
 
+
   protected function getScholarship() {
     return $this->extractValue($this->node->field_scholarship_amount);
+  }
+
+
+  protected function getSolutionData() {
+    $data['copy'] = $this->extractValue($this->node->field_solution_copy);
+    $data['support_copy'] = $this->extractValue($this->node->field_solution_support);
+
+    return $data;
+  }
+
+
+  protected function getStaffPickStatus() {
+    return (bool) $this->extractValue($this->node->field_staff_pick);
   }
 
 
@@ -120,6 +188,10 @@ class Campaign {
 
     if ($field['target_id']) {
       return $field['target_id'];
+    }
+
+    if ($field['tid']) {
+      return $field['tid'];
     }
 
     return NULL;
