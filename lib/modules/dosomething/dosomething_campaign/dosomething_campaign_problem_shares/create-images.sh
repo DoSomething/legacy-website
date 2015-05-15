924a8eef6d4cb0cdc07e4ac45725cf2a7335d8df
Shae Smith <ssmith@dosomething.org>
2015-05-15 17:33:21 -0400

    Programatically finding full path to font files

--- lib/modules/dosomething/dosomething_campaign/dosomething_campaign_problem_shares/create-images.sh
+++ lib/modules/dosomething/dosomething_campaign/dosomething_campaign_problem_shares/create-images.sh
@@ -2,23 +2,24 @@
 
 TEMP_FILE_1=tmp1.png
 TEMP_FILE_2=tmp2.png
-BASE_PATH=/var/www/dev.dosomething.org/lib/modules/dosomething/dosomething_campaign/dosomething_campaign_problem_shares/
-FINAL_FILE=$BASE_PATH/images/$1.jpg
+BOLD_FONT_PATH=`/usr/bin/find /var/www -name "ProximaNova-Bold.otf"`
+REG_FONT_PATH=`/usr/bin/find /var/www -name "ProximaNova-Reg.otf"`
+FINAL_FILE=`/usr/bin/find /var/www -type d -name "dosomething_campaign_problem_shares"`/images/$1.jpg
 
 convert \
   -size 471x248 \
   canvas:#23B7FB \
   -fill "#337D9F" \
   -pointsize 16 \
-  -font $BASE_PATH/fonts/ProximaNova-Bold.otf \
-  -annotate +20+40 'make a difference' \
+  -font $BOLD_FONT_PATH \
+  -annotate +20+40 'Make a Difference' \
   $TEMP_FILE_1
 
 convert \
   -background transparent \
   -size 431x228 \
   -fill white \
-  -font $BASE_PATH/fonts/ProximaNova-Reg.otf \
+  -font $REG_FONT_PATH \
   -pointsize 24 \
   caption:"$2" \
   $TEMP_FILE_2 \
