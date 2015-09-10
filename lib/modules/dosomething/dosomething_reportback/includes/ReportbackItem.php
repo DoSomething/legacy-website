bc6605546182b0826b6ff08c0ae02fe2c1a7d973
Chloe Lee <chloealessandra@gmail.com>
2015-09-10 14:01:30 -0400

    cleans up code and cache is working

--- lib/modules/dosomething/dosomething_reportback/includes/ReportbackItem.php
+++ lib/modules/dosomething/dosomething_reportback/includes/ReportbackItem.php
@@ -118,13 +118,14 @@ class ReportbackItem extends Entity {
 
     $northstar_user = dosomething_northstar_get_northstar_user($data->uid);
     $northstar_user = json_decode($northstar_user->data, true);
+    $northstar_user = (object) $northstar_user['data'][0];
 
     $this->user = [
       'id' => $data->uid,
-      'first_name' => $northstar_user['data'][0]['first_name'],
-      'last_name' => $northstar_user['data'][0]['last_name'],
-      'photo' => $northstar_user['data'][0]['photo'],
-      'country' => $northstar_user['data'][0]['country'],
+      'first_name' => $northstar_user->first_name,
+      'last_name' => $northstar_user->last_name,
+      'photo' => $northstar_user->photo,
+      'country' => $northstar_user->country,
     ];
   }
 
