9c568f290c9a972e8a4474c82279e2a421a41fbe
David Furnes <david@dfurnes.com>
2015-12-10 13:36:31 -0500

    Add a bit more documentation on the autoloader.

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,6 +1,14 @@
 <?php
 
 /**
- * Make the magic happen.
+ * Require Composer's class loader so we don't have to worry
+ * about manually loading any PSR-4 or vendor classes.
+ *
+ * This file is referenced from `settings.php` during Drupal's
+ * standard bootstrapping process, and directly from PHPUnit
+ * unit tests (which do *not* bootstrap the full Drupal env.)
+ *
+ * For further reading:
+ * https://getcomposer.org/doc/01-basic-usage.md#autoloading
  */
 require __DIR__ . '/../vendor/autoload.php';
