8d830241d242dd5d6743099c1d984e44b0f833e9
deadlybutter <thedeadlybutter@gmail.com>
2015-12-10 14:57:03 -0500

    Fixes typo and style ci warning

--- lib/modules/dosomething/dosomething_campaign/theme/hot-module.tpl.php
+++ lib/modules/dosomething/dosomething_campaign/theme/hot-module.tpl.php
@@ -16,7 +16,7 @@
           <canvas class="js-progress-chart" width="280" height="200" data-goal="<?php print $goal; ?>"></canvas>
         </div>
         <div class="stat-card__deadline">
-          <p><?php print t("Deadline for reaching to goal is") ?></p>
+          <p><?php print t('Deadline for reaching the goal is') ?></p>
           <p><?php print $exact_end_date ?></p>
         </div>
       </div>
