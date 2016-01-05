c23f7a0c915f9e05d0bbea2d26b433bb1ccc0e2e
Diego Lorenzo <diego@diegolorenzo.com>
2016-01-05 18:56:53 -0500

    Updating the templating process for 'photo' pattern.
    Using the ReportbackItems class for retrieving gallery items means that the structure of the data has changed from being an array to now an object with a different set of properties, etc. This updates the templating process to account for the new data structure that gets passed to the template.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/photo.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/photo.tpl.php
@@ -1,11 +1,11 @@
 <figure class="photo -stacked -framed">
-  <?php if (isset($content['admin_link'])): ?>
+  <?php if (isset($content->admin_link)): ?>
     <div class="admin-edit">
-      <a class="button -secondary" href="<?php print $content['admin_link']; ?>"><?php print t('Edit Status'); ?></a>
+      <a class="button -secondary" href="<?php print $content->admin_link; ?>"><?php print t('Edit Status'); ?></a>
     </div>
   <?php endif; ?>
-  <img src="<?php print $content['image']; ?>" alt="<?php print filter_xss($content['caption']); ?>" />
+  <img src="<?php print $content->media['uri']; ?>" alt="<?php print filter_xss($content->caption); ?>" />
   <figcaption class="__copy">
-    <?php print filter_xss($content['caption']); ?>
+    <?php print filter_xss($content->caption); ?>
   </figcaption>
 </figure>
