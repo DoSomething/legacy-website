3a2387ac96b4ff69ae3a7fb46df6c62a85901df0
Chloe Lee <chloealessandra@gmail.com>
2016-01-06 17:23:35 -0500

    adds all pseudo code and moves from own script file to dosomething_cmapaign_run.install

--- scripts/campaign-run-cleanup.php
+++ scripts/campaign-run-cleanup.php
@@ -4,5 +4,17 @@
  */
 
 // get all campaigns with status active
-$active_campaigns = db_query('SELECT nid FROM node WHERE campaign status == active');
+$active_campaigns = db_query('SELECT n.nid, n.title, n.created
+FROM node n
+INNER JOIN field_data_field_campaign_status s on s.entity_id = n.nid
+WHERE field_campaign_status_value = 'active';');
+
+
+
+// foreach actvie campaign
 // create a new node with title of campaign + current year?
+// n = new node()
+// n.title = 'whatever fantini wants'
+// n.campaign = active.nid
+// n.start_date active.created
+// n.save()
