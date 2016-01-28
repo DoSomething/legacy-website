149ad474f642760906d4e4bdd82cccd21d246b6b
Andrea Gaither <angaither@gmail.com>
2016-01-28 15:08:07 -0500

    ya ya style ci. but "\n" has to be double quotes. 😎

--- scripts/remove-null-zero-duplicate-signups.php
+++ scripts/remove-null-zero-duplicate-signups.php
@@ -7,11 +7,11 @@
  *
  */
 
-$duplicates = db_query("SELECT s.uid, s.nid, group_concat(s.sid) as sids, group_concat(ifnull(s.run_nid, 0)) as run_nids, COUNT(*) c
+$duplicates = db_query('SELECT s.uid, s.nid, group_concat(s.sid) as sids, group_concat(ifnull(s.run_nid, 0)) as run_nids, COUNT(*) c
                         FROM dosomething_signup s
                         GROUP BY s.uid, s.nid
                         HAVING c > 1
-                        ORDER BY s.nid;");
+                        ORDER BY s.nid;');
 
 foreach ($duplicates as $duplicate) {
   $sids = explode(',', $duplicate->sids);
@@ -20,7 +20,7 @@ foreach ($duplicates as $duplicate) {
     if ($run_nids[$key] == 0) {
       // Remove that row.
       db_delete('dosomething_signup')->condition('sid', $sid)->execute();
-      echo "removed row: " . $sid . "\n";
+      echo 'removed row: ' . $sid . "\n";
     }
   }
 }
