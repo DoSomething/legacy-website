680c650ed399f51711de0e197868f4ab679df7a8
Diego Lorenzo <diego@diegolorenzo.com>
2016-02-03 14:24:07 -0500

    Clear cache for API when a campaign is updated.
    This does a general clear of any campaign data in cache for the API. This also puts into use the CampaignController class that has been sitting idle. Might become more useful down the line.

--- lib/modules/dosomething/dosomething_campaign/includes/CampaignController.php
+++ lib/modules/dosomething/dosomething_campaign/includes/CampaignController.php
@@ -2,6 +2,15 @@
 
 class CampaignController {
 
-  // @TODO: handle main app controller-y things such as CRUD in here.
+  /**
+   * Update the specified Campaign in storage.
+   *
+   * @param  int  $id
+   * @return void
+   */
+  function update($id) {
+    cache_clear_all('campaigns_api_request', 'cache_dosomething_api', TRUE);
+    cache_clear_all('ds_campaign_' . $id, 'cache_dosomething_campaign', TRUE);
+  }
 
 }
