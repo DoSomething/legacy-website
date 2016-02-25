04db91f30b8d62a9430b800c06d71498fc77bbce
Andrea Gaither <angaither@gmail.com>
2016-02-25 10:30:38 -0500

    Added some logging to the run script. fixed up the query to include rbid, or sid set the run_nid as well as the nid :facepalm:

--- scripts/move-signups-reportbacks-from-runs.php
+++ scripts/move-signups-reportbacks-from-runs.php
@@ -11,20 +11,29 @@ $arg = drush_get_arguments();
 $table = 'dosomething_' . $arg[2];
 
 if (db_table_exists($table)) {
+  if ($table == 'dosomething_signup') {
+    $id = 'sid';
+  }
+  else {
+    $id = 'rbid';
+  }
   $query = db_select($table, 't');
   $query->join('field_data_field_campaigns', 'c', 't.nid = c.entity_id');
   $query->join('node', 'n', 't.nid = n.nid');
-  $results = $query->fields('t', ['nid'])
+  $query->fields('t', ['nid'])
   ->fields('c', ['field_campaigns_target_id'])
-  ->condition('n.type', 'campaign_run')
+  ->fields('t', [$id])
+  ->addField('t', $id, 'id');
+  $results = $query->condition('n.type', 'campaign_run')
   ->execute();
 
   foreach($results as $result) {
     try{
       db_update($table)
-      ->fields(['nid' => $result->field_campaigns_target_id])
-      ->condition('nid', $result->nid)
+      ->fields(['nid' => $result->field_campaigns_target_id, 'run_nid' => $result->nid])
+      ->condition($id, $result->id)
       ->execute();
+      echo "updated : " . $result->id . "\n";
     }
     catch(Exception $e) {
       echo 'bad things: ' . $e;
