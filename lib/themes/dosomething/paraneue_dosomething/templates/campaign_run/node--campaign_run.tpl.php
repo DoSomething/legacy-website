5359bd3eaed1fee5241b336009b809716539faaf
Chloe Lee <chloealessandra@gmail.com>
2016-04-01 14:06:54 -0400

    adds add buttons to winners and klout gallery sections

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
@@ -9,7 +9,15 @@
 </h4>
 
 <h4>Winners:</h4>
+  <p><?php print l('Add', '/field-collection/field-winners/add/node/' . $nid . '/?destination=node/' . $nid); ?></p>
+
 <h4>Klout Gallery:</h4>
+  <p><?php print l('Add', '/field-collection/field-gallery-collection/add/node/' . $nid . '/?destination=node/' . $nid); ?></p>
+
 <h4>Run Date: 
   <p><?php print t($run_date); ?></p>
-</h4>
+</h4
+
+
+
+
