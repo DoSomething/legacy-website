2bb987c7d46dfe1f0d94ce238ea300607800e296
Chloe Lee <chloealessandra@gmail.com>
2016-04-01 16:00:56 -0400

    gets rid of custom build of form and instead just renders page content

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
@@ -3,21 +3,14 @@
  * Returns the HTML for the Campaign Run page.
  */
 ?>
-
 <h4>Campaign: 
   <p><?php print l($campaign_title, 'node/' . $campaign_nid); ?></p>
 </h4>
 
-<h4>Winners:</h4>
-  <p><?php print l('Add', '/field-collection/field-winners/add/node/' . $nid . '/?destination=node/' . $nid); ?></p>
-
-<h4>Klout Gallery:</h4>
-  <p><?php print l('Add', '/field-collection/field-gallery-collection/add/node/' . $nid . '/?destination=node/' . $nid); ?></p>
-
-<h4>Run Date: 
-  <p><?php print t($run_date); ?></p>
-</h4
-
-
+<?php 
+  print render($content['field_winners']); 
+  print render($content['field_gallery_collection']);
+  print render($content['field_run_date']);
+?>
 
 
