be7475744630f50666ef502bccc4ebda0bf097ce
Chloe Lee <chloealessandra@gmail.com>
2016-04-01 13:47:14 -0400

    adds run date to campaign run template

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
@@ -4,4 +4,12 @@
  */
 ?>
 
-<p>Campaign: <?php print l($campaign_title, 'node/' . $campaign_nid); ?></p>
+<h4>Campaign: 
+  <p><?php print l($campaign_title, 'node/' . $campaign_nid); ?></p>
+</h4>
+
+<h4>Winners:</h4>
+<h4>Klout Gallery:</h4>
+<h4>Run Date: 
+  <p><?php print t($run_date); ?></p>
+</h4>
