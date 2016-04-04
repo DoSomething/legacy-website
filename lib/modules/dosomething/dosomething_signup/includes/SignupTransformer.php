d558441406e4b040d5f75047731be621b3ae5bba
Chloe Lee <chloealessandra@gmail.com>
2016-04-04 16:55:33 -0400

    runs param works for signups endpoint

--- lib/modules/dosomething/dosomething_signup/includes/SignupTransformer.php
+++ lib/modules/dosomething/dosomething_signup/includes/SignupTransformer.php
@@ -119,6 +119,7 @@ class SignupTransformer extends Transformer {
       'count' => (int) $parameters['count'] ?: 25,
       'page' => (int) $parameters['page'],
       'competition' => $parameters['competition'] ?: NULL,
+      'runs' => dosomething_helpers_format_data($parameters['runs']),
     ];
 
     return $filters;
