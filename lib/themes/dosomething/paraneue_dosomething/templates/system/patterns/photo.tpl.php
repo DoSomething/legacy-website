81f9db7cdbf6053bdbac8e6a84c7e06117eea66a
David Furnes <david@dfurnes.com>
2016-05-10 14:18:50 -0400

    Remove share buttons and school from gallery.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/photo.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/photo.tpl.php
@@ -7,16 +7,9 @@
   <img src="<?php print $content->media['uri']; ?>" alt="<?php print filter_xss($content->caption); ?>" />
   <figcaption class="__copy">
     <p class="photo__caption"><?php print filter_xss($content->caption); ?></p>
-    <?php $schools = ['Masuk High School', 'Chalk Hill Middle School', 'University of Connecticut', 'Fawn Hollow Elementary School', 'New York University', 'Columbia University']; ?>
-    <div class="footnote"><?php print $schools[array_rand($schools)] ?></div>
   </figcaption>
   <div class="form-actions -inline photo--actions">
     <li><a class="button -mini js-kudos-like-button">Eat it!</a> <span class="counter"><?php print rand(13, 1042) ?></span></li>
     <li><a class="button -mini js-kudos-trash-button">Toss it!</a> <span class="counter"><?php print rand(13, 1042) ?></span></li>
-    <ul class="photo--shares">
-      <li><a href="https://www.facebook.com/share.php?u=http://staging.beta.dosomething.org/reportback/1299" class="social-icon -facebook -secondary js-share-link"><span>Facebook</span></a></li>
-      <li><a href="#" class="social-icon -twitter -secondary js-share-link"><span>Twitter</span></a></li>
-      <li><a href="#" class="social-icon -tumblr -secondary js-share-link"><span>Tumblr</span></a></li>
-    </ul>
   </div>
 </figure>
