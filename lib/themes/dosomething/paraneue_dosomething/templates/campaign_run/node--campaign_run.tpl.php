d5814627fb89bda623985fd49d410980908b1e45
Chloe Lee <chloealessandra@gmail.com>
2016-06-02 14:23:57 -0400

    adds description placeholder on campaign run view

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
@@ -1,17 +1,21 @@
-<?php 
+<?php
 /**
  * Returns the HTML for the Campaign Run page.
  */
 ?>
-<h4>Campaign: 
+<h4>Campaign:
   <p><?php print l($campaign_title, 'node/' . $field_campaigns[0]['target_id']); ?></p>
+</h4>
+
+<h4>Run Description:
+  <p><?php print render($field_instances['node-campaign_run-description_field']['description']); ?></p>
 </h4>
 
 <h4>Campaign Closed State:
   <p><?php print l($campaign_title . ': Closed State', 'node/' . $field_campaigns[0]['target_id'] . '/closed'); ?></p>
 </h4>
 
-<?php 
+<?php
   print render($content['field_winners']);
   print render($content['field_gallery_collection']);
   print render($content['field_run_date']);
