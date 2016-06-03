81780ad2644659cad00b74068acca9efc07a74f0
Chloe Lee <chloealessandra@gmail.com>
2016-06-03 12:09:32 -0400

    adds description box in basic info section

--- lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/campaign_run/node--campaign_run.tpl.php
@@ -7,12 +7,12 @@
   <p><?php print l($campaign_title, 'node/' . $field_campaigns[0]['target_id']); ?></p>
 </h4>
 
-<h4>Run Description:
-  <p><?php print render($field_instances['node-campaign_run-description_field']['description']); ?></p>
-</h4>
-
 <h4>Campaign Closed State:
   <p><?php print l($campaign_title . ': Closed State', 'node/' . $field_campaigns[0]['target_id'] . '/closed'); ?></p>
+</h4>
+
+<h4>Run Description:
+  <p><?php print_r($field_campaigns); ?></p>
 </h4>
 
 <?php
