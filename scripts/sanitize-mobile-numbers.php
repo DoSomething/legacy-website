ca573cd3cfe4f6b3d690d509377c8206842a923a
David Furnes <david@dfurnes.com>
2016-08-16 12:13:16 -0400

    Don't escape regex. Credit for fix to @angaither.
    See: http://stackoverflow.com/a/9033613/811624

--- scripts/sanitize-mobile-numbers.php
+++ scripts/sanitize-mobile-numbers.php
@@ -8,9 +8,9 @@
  *
  */
 
-$wild_typers = db_query("SELECT entity_id as uid, field_mobile_value as mobile
+$wild_typers = db_query('SELECT entity_id as uid, field_mobile_value as mobile
                         FROM field_data_field_mobile
-                        WHERE field_mobile_value NOT REGEXP '^[0-9]{10}$';");
+                        WHERE field_mobile_value NOT REGEXP :regex', [':regex' => '^[0-9]+$']);
 
 foreach($wild_typers as $wilder) {
   if ($wilder->mobile) {
