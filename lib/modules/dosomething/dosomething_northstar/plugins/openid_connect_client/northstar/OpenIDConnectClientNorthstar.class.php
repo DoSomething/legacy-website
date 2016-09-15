f8564cea5927af60356bfa1a319d814caefcc09d
David Furnes <david@dfurnes.com>
2016-09-15 16:40:27 -0400

    Parse the refresh token and add hook to persist it.

--- lib/modules/dosomething/dosomething_northstar/plugins/openid_connect_client/northstar/OpenIDConnectClientNorthstar.class.php
+++ lib/modules/dosomething/dosomething_northstar/plugins/openid_connect_client/northstar/OpenIDConnectClientNorthstar.class.php
@@ -33,7 +33,46 @@ class OpenIDConnectClientNorthstar extends OpenIDConnectClientBase {
   }
 
   /**
-   * Implements OpenIDConnectClientInterface::decodeIdToken().
+   * Overrides OpenIDConnectClientBase::retrieveIDToken().
+   */
+  public function retrieveTokens($authorization_code) {
+    $redirect_uri = OPENID_CONNECT_REDIRECT_PATH_BASE . '/' . $this->name;
+
+    $post_data = [
+      'grant_type' => 'authorization_code',
+      'client_id' => $this->getSetting('client_id'),
+      'client_secret' => $this->getSetting('client_secret'),
+      'redirect_uri' => url($redirect_uri, ['absolute' => TRUE]),
+      'code' => $authorization_code,
+    ];
+
+    $request_options = [
+      'method' => 'POST',
+      'data' => drupal_http_build_query($post_data),
+      'timeout' => 15,
+      'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+    ];
+
+    $endpoints = $this->getEndpoints();
+    $response = drupal_http_request($endpoints['token'], $request_options);
+
+    if (! empty($response->error) || $response->code != 200) {
+      openid_connect_log_request_error(__FUNCTION__, $this->name, $response);
+      return FALSE;
+    }
+
+    $response_data = drupal_json_decode($response->data);
+
+    return [
+      'id_token' => $response_data['access_token'], // id_token
+      'access_token' => $response_data['access_token'],
+      'refresh_token' => $response_data['refresh_token'],
+      'expire' => REQUEST_TIME + $response_data['expires_in'],
+    ];
+  }
+
+  /**
+   * Overrides OpenIDConnectClientBase::decodeIdToken().
    */
   public function decodeIdToken($id_token) {
     // @TODO: Verify JWT using public key.
