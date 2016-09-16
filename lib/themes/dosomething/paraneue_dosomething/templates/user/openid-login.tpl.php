5c41901653ff6db989f2e6054fe6406b8fcfab6d
David Furnes <david@dfurnes.com>
2016-09-16 11:05:10 -0400

    Render the form on the OpenID connect page.

--- lib/themes/dosomething/paraneue_dosomething/templates/user/openid-login.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/user/openid-login.tpl.php
@@ -1,3 +1,9 @@
 <h3>Welcome to the OpenID Login brought to you by Northstar™.</h3>
 
-<p>Login form goes here!</p>
+<?php print render($form); ?>
+
+<div class="message-callout -below">
+  <div class="message-callout__copy">
+    <p>Click the button!</p>
+  </div>
+</div>
