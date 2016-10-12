5659e91874cd95bd928e4e0685ff6990a1427b19
Chloe Lee <chloealessandra@gmail.com>
2016-10-12 15:04:56 -0400

    adds PhoenixTransactionBridge.php

---
+++ lib/app/Auth/PhoenixTransactionBridge.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phoenix\Auth;
+
+use DoSomething\Gateway\Contracts\TransactionBridgeContract; // we import the transaction bridge contract
+use League\OAuth2\Client\Token\AccessToken;
+
+class PhoenixTransactionBridge implements TransactionBridgeContract {
+
+  /**
+   * Get the value of the given HTTP header.
+   *
+   * @return string
+   */
+  public function getHeader($name) {
+      return drupal_get_http_header($name);
+  }
+
+  /**
+   * Write a log message.
+   *
+   * @return void
+   */
+  public function log($message, array $details) {
+      watchdog('Transaction', $details, [], WATCHDOG_INFO);
+  }
+}
