6791dfe0e3d38bc3e904d5d6f754eaf476a0e00d
Chloe Lee <chloealessandra@gmail.com>
2016-10-13 16:13:43 -0400

    adds doc blocks in Rogue.php and checks if response is null

--- lib/modules/dosomething/dosomething_rogue/includes/Rogue.php
+++ lib/modules/dosomething/dosomething_rogue/includes/Rogue.php
@@ -6,7 +6,43 @@ use DoSomething\Gateway\ForwardsTransactionIds;
 class Rogue extends RestApiClient {
   use ForwardsTransactionIds;
 
+  /**
+   * Get the CSRF token for the authenitcated API session.
+   * The class name of the transaction framework bridge.
+   *
+   * @return string - token
+   * @var string
+   */
+  private function getAuthenticationToken()
+  {
+    return $this->authenticate()['token'];
+  }
+
+  protected $transactionBridge = Phoenix\Auth\PhoenixTransactionBridge::class;
+
+  /**
+   * Send a POST request of the reportback to be saved in Rogue.
+   *
+   * @param string $url
+   * @param array $data
+   * @return object|false
+   */
   public function postReportback($url, $data) {
-    return $this->post($url, $data);
+    $respons = $this->post($url, $data);
+
+    return is_null($response) ? null : $response;
+  }
+
+  /**
+   * Send a PUT request of the updated reportback to be saved in Rogue.
+   *
+   * @param string $url
+   * @param array $data
+   * @return object|false
+   */
+  public function updateReportback($url, $data) {
+    $response = $this->put($url, $data);
+
+    return is_null($response) ? null : $response;
   }
 }
