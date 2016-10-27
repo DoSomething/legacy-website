e0dd5420c772e33089b7c241fdce6f1c7dab804a
katiecrane <kcrane@dosomething.org>
2016-10-27 11:56:46 -0400

    update vars used on fail page

--- lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-rogue-fail-permalink.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-rogue-fail-permalink.tpl.php
@@ -5,14 +5,10 @@
  *
  * Available variables:
  * - $node->title                        : Title of the campaign
- * - $is_owner                           : Used to determine if the curernt logged in user is the user who submitted the rb.
+ * - $fail_image                         : Image to use in place of reportback photo
  * - $title                              : Title of the permalink/confirmation page.
  * - $subtitle                           : Subtitle of the permalink/confirmation page.
- * - $copy_vars['owners_rb_scholarship'] : Subtitle of the reportback confirmation page.
- * - $copy_vars['owners_rb_important']   : Title of the why I participated section
- * - $user->first_name                   : User's first name
- * - $node->fact_problem['fact']
- * - $node->fact_solution['fact']
+ * - $failure_copy                       : Text to use next to the image (in place of the user's Why Participated)
  *
  */
 
