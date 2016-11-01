ed50659f81c14917a5de67efa67fb7c98caa05eb
deadlybutter <thedeadlybutter@gmail.com>
2016-11-01 14:25:11 -0400

    Adds complete registration tracking

--- lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
@@ -65,6 +65,7 @@
     document,'script','https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '883788485091123');
     fbq('track', 'PageView');
+    fbq('track', 'CompleteRegistration');
     </script>
     <noscript><img height="1" width="1" style="display:none"
     src="https://www.facebook.com/tr?id=883788485091123&ev=PageView&noscript=1"
