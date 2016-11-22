7bb27954c12604ab3acfc921cbd4beeefc6d4a65
Andrea Gaither <angaither@gmail.com>
2016-11-22 16:07:09 -0500

    Update the data array to include most things. For now just print it to ensure it's correct.

--- scripts/export-reportbacks-to-rogue.php
+++ scripts/export-reportbacks-to-rogue.php
@@ -7,11 +7,14 @@
  */
 
 include_once('../lib/modules/dosomething/dosomething_rogue/dosomething_rogue.module');
+include_once('../lib/modules/dosomething/dosomething_northstar/dosomething_northstar.module');
+
 
 $last_saved = variable_get('dosomething_rogue_last_rbi_migrated', NULL);
 if ($last_saved) {
   $rbis = db_query("SELECT *
             FROM dosomething_reportback_file rbf
+            LEFT JOIN dosomething_reportback rb on rbf.rbid = rb.rbid
             WHERE rbf.fid > $last_saved
             ORDER BY rbf.fid");
 }
@@ -19,22 +22,35 @@ else {
   // Get all the users!
   $rbis = db_query('SELECT *
                    FROM dosomething_reportback_file rbf
+                   LEFT JOIN dosomething_reportback rb on rbf.rbid = rb.rbid
                    ORDER BY rbf.fid');
 }
 
-  $client = dosomething_rogue_client();
-  echo 'Client ' . $client;
+$client = dosomething_rogue_client();
+
 
 foreach ($rbis as $rbi) {
-  // Get the reportback item and info
 
-  // Get the reportback continer info
+  $data = [
+    'northstar_id' => dosomething_northstar_get_user($rb->uid, 'drupal_id'),
+    'campaign_id' => $rb->nid,
+    'campaign_run_id' => $rb->run_nid,
+    'quantity' => $rb->quantity,
+    'why_participated' => $rb->why_participated,
+    'file' => dosomething_helpers_get_data_uri_from_fid($rb->fid),
+    'caption' => $rb->caption,
+    'source' => $rb->source,
+    'remote_addr' => $rb->remote_addr,
+    // do this but first need to transform what they mean.
+    // 'status' => isset($values['status']) ? $values['status'] : 'pending',
+  ];
+
 
   // Transform Reviewer to NS id
   // Transform user to NS id
   // Transform status to the rogue status
 
-  echo $rbi->fid . "\n";
+  print_r($data) . "\n";
 
   // $response = $client->postReportback($data);
 
