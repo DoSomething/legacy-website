d54a9e9fab151de77f31a1b114e76c01698d4cc7
Andrea Gaither <angaither@gmail.com>
2016-12-02 14:54:28 -0500

    Updated the script to - check for files before sending them to rogue - only send request to rogue if NS id exists

--- scripts/export-reportbacks-to-rogue.php
+++ scripts/export-reportbacks-to-rogue.php
@@ -15,6 +15,7 @@ if ($last_saved) {
   $rbis = db_query("SELECT *
             FROM dosomething_reportback_file rbf
             INNER JOIN dosomething_reportback rb on rbf.rbid = rb.rbid
+            INNER JOIN file_managed fm on fm.fid = rbf.fid
             WHERE rbf.fid > $last_saved
             AND rbf.fid NOT IN (SELECT fid FROM dosomething_rogue_reportbacks)
             ORDER BY rbf.fid");
@@ -24,6 +25,7 @@ else {
   $rbis = db_query('SELECT *
                    FROM dosomething_reportback_file rbf
                    INNER JOIN dosomething_reportback rb on rbf.rbid = rb.rbid
+                   INNER JOIN file_managed fm on fm.fid = rbf.fid
                    WHERE rbf.fid NOT IN (SELECT fid FROM dosomething_rogue_reportbacks)
                    ORDER BY rbf.fid');
 }
@@ -31,36 +33,40 @@ else {
 $client = dosomething_rogue_client();
 
 foreach ($rbis as $rb) {
-  //@todo? Set a variable or header to disable the send back to phoenix for these.
+  $northstar_id = dosomething_northstar_get_user($rb->uid, 'drupal_id');
+  if (isset($northstar_id)) {
+    $data = [
+      'northstar_id' => $northstar_id->id,
+      'drupal_id' => $rb->uid,
+      'campaign_id' => $rb->nid,
+      'campaign_run_id' => $rb->run_nid,
+      'quantity' => $rb->quantity,
+      'why_participated' => $rb->why_participated,
+      'file' => dosomething_helpers_get_data_uri_from_fid($rb->fid),
+      'caption' => $rb->caption,
+      'source' => $rb->source,
+      'remote_addr' => $rb->remote_addr,
+      'status' => dosomething_rogue_transform_status($rb->status),
+      // Tell rogue not to try to forward the reportback back to phoenix.
+      'do_not_forward' => TRUE,
+    ];
 
-  $data = [
-    'northstar_id' => dosomething_northstar_get_user($rb->uid, 'drupal_id')->id,
-    'drupal_id' => $rb->uid,
-    'campaign_id' => $rb->nid,
-    'campaign_run_id' => $rb->run_nid,
-    'quantity' => $rb->quantity,
-    'why_participated' => $rb->why_participated,
-    'file' => dosomething_helpers_get_data_uri_from_fid($rb->fid),
-    'caption' => $rb->caption,
-    'source' => $rb->source,
-    'remote_addr' => $rb->remote_addr,
-    'status' => dosomething_rogue_transform_status($rb->status),
-  ];
+    try {
+      $response = $client->postReportback($data);
+      // Output progress to see what's going on.
+      echo 'Migrated reportback ' . $rb->fid . ' to Rogue [' . $rb->caption . ']' . PHP_EOL;
 
-  try {
-    $response = $client->postReportback($data);
+      // Store the reference in dosomething_rogue_reportbacks.
+      dosomething_rogue_store_rogue_references($rb->rbid, $rb->fid, $response);
 
-    // Output progress to see what's going on.
-    echo 'Migrated reportback ' . $rb->fid . ' to Rogue [' . $rb->caption . ']' . PHP_EOL;
+      // If the script fails, we can use this to start the script from the last reportback migrated.
+      variable_set('dosomething_rogue_last_rbi_migrated', $rb->fid);
+    } catch (GuzzleHttp\Exception\ServerException $e) {
+      // @todo do we care about this?
+      echo 'Something guzzle and terrible ' . $rb->fid . PHP_EOL;
 
-    // Store the reference in dosomething_rogue_reportbacks.
-    dosomething_rogue_store_rogue_references($rb->rbid, $rb->fid, $response);
-
-    // If the script fails, we can use this to start the script from the last reportback migrated.
-    variable_set('dosomething_rogue_last_rbi_migrated', $rb->fid);
-  } catch (GuzzleHttp\Exception\ServerException $e) {
-    // @todo handle failed migrations.
-
-    echo 'Something terrible '  . $rb->fid  . PHP_EOL;
+    }
   }
+  echo 'No northstar id, that is terrible ' . $rb->uid . PHP_EOL;
+
 }
