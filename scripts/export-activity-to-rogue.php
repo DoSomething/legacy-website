c74b6b1f71315ddc1d532ce2cdbd90c68eb33e57
katiecrane <kcrane@dosomething.org>
2017-02-10 14:41:52 -0500

    Refactor and timestamp progress

--- scripts/export-activity-to-rogue.php
+++ scripts/export-activity-to-rogue.php
@@ -12,98 +12,100 @@ include_once('../lib/modules/dosomething/dosomething_northstar/dosomething_north
 // Pick up from where we left off, if we want to
 // $last_saved = variable_get('dosomething_rogue_last_rbi_migrated', NULL);
 
-// Get ALL the things!
+// Pick up getting ALL the things from where we left off!
 if ($last_saved) {
-  $signups = db_query("SELECT signups.sid, signups.uid, signups.nid, signups.run_nid, signups.source, signups.timestamp, rb.quantity, rb.why_participated, rb.rbid, rb.flagged, rb.updated, rbf.fid, rbf.remote_addr, rbf.caption, rbf.status, rbf.reviewed, rbf.reviewer, rbf.source
+  $signups = db_query("SELECT signups.sid, signups.uid, signups.nid, signups.run_nid, signups.source, signups.timestamp, rb.quantity, rb.why_participated, rb.rbid, rb.flagged, rb.updated
                       FROM dosomething_signup signups
                       LEFT JOIN dosomething_reportback rb ON signups.uid = rb.uid AND signups.nid = rb.nid AND signups.run_nid = rb.run_nid
-                      LEFT JOIN dosomething_reportback_file rbf on rbf.rbid = rb.rbid
+                      WHERE signups.sid NOT IN (SELECT sid FROM dosomething_rogue_signups)
                       ORDER BY signups.sid
-                      LIMIT 30");
+                      LIMIT 1");
 }
 else {
-  // Get all the things!
-  $signups = db_query('SELECT signups.sid, signups.uid, signups.nid, signups.run_nid, signups.source, signups.timestamp, rb.quantity, rb.why_participated, rb.rbid, rb.flagged, rb.updated, rbf.fid, rbf.remote_addr, rbf.caption, rbf.status, rbf.reviewed, rbf.reviewer, rbf.source
+  // Get ALL the things!
+  $signups = db_query('SELECT signups.sid, signups.uid, signups.nid, signups.run_nid, signups.source, signups.timestamp, rb.quantity, rb.why_participated, rb.rbid, rb.flagged, rb.updated
                       FROM dosomething_signup signups
                       LEFT JOIN dosomething_reportback rb ON signups.uid = rb.uid AND signups.nid = rb.nid AND signups.run_nid = rb.run_nid
-                      LEFT JOIN dosomething_reportback_file rbf on rbf.rbid = rb.rbid
+                      WHERE signups.sid NOT IN (SELECT sid FROM dosomething_rogue_signups)
                       ORDER BY signups.sid
-                      LIMIT 30')->fetchAll();
+                      LIMIT 1')->fetchAll();
 }
 
-// $client = dosomething_rogue_client();
-$current_sid = 0;
+$client = dosomething_rogue_client();
 
 foreach ($signups as $signup) {
-  echo 'Trying sid ' . $signup->sid . ' and fid ' . $signup->fid . '...' . PHP_EOL;
-  
-  // If we have already started this signup, just add the new photo (all other data is already taken care of)
-  if ($signup->sid == $current_sid) {
-    // Increment the item counter
-    $photo = $photo + 1;
+  echo 'Trying sid ' . $signup->sid . '...' . PHP_EOL;
 
-    // Add the current item
-    $data['photo'][$photo] = [
-      'source' => $signup->source,
-      'remote_addr' => $signup->remote_addr,
-      'caption' => $signup->caption,
-      'event_type' => 'post_photo',
+  $northstar_id = dosomething_northstar_get_user($signup->uid, 'drupal_id');
+
+  // Only try to send to Rogue if we have a Northstar ID
+  if (isset($northstar_id)) {
+    // Match Rogue's timestamp format
+    $created_at = date('Y-m-d H:i:s', $signup->timestamp);
+
+    // Format data as Rogue expects
+    $data = [
+      // signup
       'northstar_id' => $northstar_id->id,
+      'campaign_id' => $signup->nid,
+      'campaign_run_id' => $signup->run_nid,
       'do_not_forward' => TRUE,
-      'file' => dosomething_helpers_get_data_uri_from_fid($signup->fid),
+      // @TODO: add timestamps (might still need to format them)
+      'created_at' => $created_at,
+      'updated_at' => $created_at, // No way to know when quantity or why_participated were last updated
     ];
+
+    // Include all the Reportback data and files if they exist
+    if ($signup->rbid) {
+      $data['quantity'] = $signup->quantity;
+      $data['why_participated'] = $signup->why_participated;
+      $data['updated_at'] = $signups->updated;
+
+      // Get the files for the Reportback
+      $photos = db_query('SELECT rbf.fid, rbf.remote_addr, rbf.caption, rbf.status, rbf.reviewed, rbf.reviewer, rbf.source, rblog.timestamp
+                          FROM dosomething_reportback_file rbf
+                          INNER JOIN dosomething_reportback_log rblog ON rbf.fid = substring_index(rblog.files, \',\',-1)
+                          WHERE rbf.rbid = ' . $signup->rbid)->fetchAll();
+
+      // Format the photos to send to Rogue
+      foreach ($photos as $key=>$photo) {
+        echo "\t" . 'Trying fid ' . $photo->fid . '...' . PHP_EOL;
+        echo "\t\t" . 'got timestamp for file of ' . $photo->timestamp . PHP_EOL;
+
+        // Match Rogue's timestamp format
+
+        $data['photo'][$key] = [
+          'source' => $photo->source,
+          'remote_addr' => $photo->remote_addr,
+          'caption' => $photo->caption,
+          'event_type' => 'post_photo',
+          'northstar_id' => $northstar_id->id,
+          'do_not_forward' => TRUE,
+          'file' => dosomething_helpers_get_data_uri_from_fid($photo->fid),
+          // @TODO: figure out timestamp situation
+        ];
+      }
+
+    }
+  echo 'Request:' . PHP_EOL . json_encode($data) . PHP_EOL;
+
+    // catch GuzzleHttp\Exception\ServerException? make sure Rogue is handling null images first
+    // Send the request to Rogue
+    $response = $client->postSignup($data);
+
+    // Make sure we get a successful response
+    if ($response) {
+      echo 'Migrated signup ' . $signup->sid . ' reportback ' . $signup->rbid . ' to Rogue.' . PHP_EOL;
+
+      // Store signup reference
+      // function dosomething_rogue_store_rogue_signup_references($sid, $rogue_signup) {
+      dosomething_rogue_store_rogue_signup_references($signup->sid, $response);
+
+      // Store reportback reference
+      // @TODO: new helper/rework current one to work with the response we get back from Rogue when posting a signup
+    }
   }
   else {
-    // @TODO:
-    // 1. send the current request to Rogue
-    // for now I am just printing out the requests to see if they look okay before flooding crap data to my local Rogue
-      echo 'Request for sid ' . $current_sid . PHP_EOL;
-      echo json_encode($data) . PHP_EOL;
-    // 2. wipe the current request
-      $data = '';
-
-    // 3. create new request
-      $northstar_id = dosomething_northstar_get_user($signup->uid, 'drupal_id');
-
-      // Only try to send to Rogue if we have a Northstar ID
-      if (isset($northstar_id)) {
-        // Format data as Rogue expects
-        $data = [
-          // signup
-          'northstar_id' => $northstar_id->id,
-          'campaign_id' => $signup->nid,
-          'campaign_run_id' => $signup->run_nid,
-          'do_not_forward' => TRUE,
-          // @TODO: add timestamps
-          'created_at' => $signups->timestamp,
-          'updated_at' => $signups->updated,
-        ];
-
-        // Add the first photo (if there is one)
-        if ($signup->fid) {
-          $data['quantity'] = $signup->quantity;
-          $data['why_participated'] = $signup->why_participated;
-
-          // Item counter
-          $photo = 0;
-
-          // Add the current item
-          $data['photo'][$photo] = [
-            'source' => $signup->source,
-            'remote_addr' => $signup->remote_addr,
-            'caption' => $signup->caption,
-            'event_type' => 'post_photo',
-            'northstar_id' => $northstar_id->id,
-            'do_not_forward' => TRUE,
-            'file' => dosomething_helpers_get_data_uri_from_fid($signup->fid),
-          ];
-        }
-      }
-      else {
-        echo 'No northstar id, that is terrible ' . $rb->uid . PHP_EOL;
-      }
-
-    // 4. set $current_sid to $signup->sid
-      $current_sid = $signup->sid;
+    echo 'No northstar id, that is terrible ' . $rb->uid . PHP_EOL;
   }
 }
