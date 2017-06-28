fd211bdba5e1c1991e1a5fc468832f7e56790be1
Chloe Lee <chloealessandra@gmail.com>
2017-06-28 12:23:54 -0400

    adds placeholder text for simple permalink page

---
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-simple-permalink.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file
+ * Reportback confirmation/permalink page for Rogue.
+ */
+?>
+<article class="reportback__permalink">
+  <header role="banner" class="header ">
+    <div class="wrapper">
+      <h1 class="header__title">Thanks for reporting back!</h1>
+      <p class="header__subtitle">We got your photo.</p>
+    </div>
+  </header>
+
+  <div class="container -padded -dark">
+    <div class="wrapper">
+      <div class="card">
+        <div class="card__column">
+          IMAGE HERE
+        </div>
+        <div class="card__column">
+          <div class="wrapper">
+          <!--Show user the reportback confirmation page -->
+            <div class="card__copy">
+              <h1>LUKE TEXT HERE</h1>
+              LUKE LOVES THIS PHOTO
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</article>
