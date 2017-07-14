c8231376c6405f2897e99b7614ff4b5f3ed6124b
Chloe Lee <chloealessandra@gmail.com>
2017-07-14 12:30:35 -0400

    adds new copy

--- lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-simple-permalink.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/reportback/reportback-simple-permalink.tpl.php
@@ -7,8 +7,8 @@
 <article class="reportback__permalink">
   <header role="banner" class="header ">
     <div class="wrapper">
-      <h1 class="header__title">Awesome! We got your photo.</h1>
-      <p class="header__subtitle">Your photo will show up in the gallery soon!</p>
+      <h1 class="header__title">Thanks for proving your impact!</h1>
+      <p class="header__subtitle">A DoSomething staffer will review your photo shortly. Hang tight!</p>
     </div>
   </header>
 
@@ -23,7 +23,7 @@
           <!--Show user the reportback confirmation page -->
             <div class="card__copy">
               <h1>What's next?</h1>
-              <p>The fun doesn't have to stop now. You can upload more photos, continue working on the campaign, or share it with your friends on social media.</p>
+              <p>Once a staffer approves your photo, it’ll appear in the gallery in the Prove It section of the campaign page. Remember, you can continue working on the campaign and update your photo and impact # as many times as you want. Let’s Do This!</p>
             </div>
           </div>
         </div>
