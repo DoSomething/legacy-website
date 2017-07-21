fa6a42b4d484b2fb797542233e2d5decfa547ed1
Chloe Lee <chloealessandra@gmail.com>
2017-07-21 19:56:00 -0400

    all works except for current user filled in heart
    gets reactions to work but now not sending to Rogue

--- lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/figure.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/patterns/figure.tpl.php
@@ -28,7 +28,11 @@
     <?php if (isset($content['kudos']) && isset($content['kudos']['fid'])): ?>
       <ul class="form-actions -inline kudos" <?php print $content['kudos']['fid'] ? 'data-reportback-item-id="' . $content['kudos']['fid'] . '"' : ''; ?>>
         <li>
-          <button class="js-kudos-button kudos__icon <?php print dosomething_kudos_term_is_selected($content['kudos'], 'heart') ? 'is-active' : '' ?>" data-kudos-term-id="<?php print $content['kudos']['allowed_reactions'][0]; ?>" data-kid="<?php print array_key_exists($content['kudos']['allowed_reactions'][0], $content['kudos']['existing_kids']) ? dosomething_helpers_isset($content['kudos']['existing_kids'][$content['kudos']['allowed_reactions'][0]], 'kid') : false ?>"></button>
+          <?php if (variable_get('rogue_collection', FALSE)): ?>
+            <button class="js-kudos-button kudos__icon <?php print $content['kudos']['liked_in_rogue'] ? 'is-active' : '' ?>" data-kudos-term-id="1" data-kid="<?php print $content['kudos']['liked_in_rogue'] ? $content['kudos']['fid'] : null ?>"></button>
+          <?php else: ?>
+            <button class="js-kudos-button kudos__icon <?php print dosomething_kudos_term_is_selected($content['kudos'], 'heart') ? 'is-active' : '' ?>" data-kudos-term-id="<?php print $content['kudos']['allowed_reactions'][0]; ?>" data-kid="<?php print array_key_exists($content['kudos']['allowed_reactions'][0], $content['kudos']['existing_kids']) ? dosomething_helpers_isset($content['kudos']['existing_kids'][$content['kudos']['allowed_reactions'][0]], 'kid') : false ?>"></button>
+          <?php endif; ?>
           <span class="counter"><?php print $content['kudos']['reaction_totals']['heart']; ?></span>
         </li>
       </ul>
