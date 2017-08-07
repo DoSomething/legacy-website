f6e908b3c796008a03d20aa2937a44a64267ef92
katiecrane <kcrane@dosomething.org>
2017-08-07 16:44:52 -0400

    Finish logic and add more comments

--- scripts/tag-as-excluded-promoted-in-rogue.php
+++ scripts/tag-as-excluded-promoted-in-rogue.php
@@ -5,7 +5,7 @@
  * RBs "promoted" here will be tagged with "Good Photo" in Rogue
  *
  * to run:
- * drush --script-path=../scripts/ php-script export-activity-to-rogue.php
+ * drush --script-path=../scripts/ php-script tag-as-excluded-promoted-in-rogue.php
  */
 
 // Pick up from where we left off
@@ -13,12 +13,12 @@ $last_promoted = variable_get('dosomething_rogue_last_post_promoted', NULL);
 $last_excluded = variable_get('dosomething_rogue_last_post_excluded', NULL);
 
 // Get all the promoted posts to send
-if ($last_promoted) {
+if (isset($last_promoted)) {
 	$promoted_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
 										FROM dosomething_reportback_file f
 										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
 										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
-										WHERE status = \"promoted\"
+										WHERE f.status = 'promoted'
 										AND r.rogue_post_id > $last_promoted
 										ORDER BY r.rogue_post_id");
 }
@@ -27,79 +27,98 @@ else {
 										FROM dosomething_reportback_file f
 										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
 										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
-										WHERE status = \"promoted\"
+										WHERE f.status = \"promoted\"
 										ORDER BY r.rogue_post_id");
 }
 
 // Get all the excluded posts to send
-if ($last_excluded) {
-	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
-										FROM dosomething_reportback_file f
-										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
-										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
-										WHERE status = \"excluded\"
-										AND r.rogue_post_id > $last_excluded
-										ORDER BY r.rogue_post_id");
-}
-else {
-	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
-										FROM dosomething_reportback_file f
-										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
-										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
-										WHERE status = \"excluded\"
-										ORDER BY r.rogue_post_id");
-}
+// if ($last_excluded) {
+// 	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
+// 										FROM dosomething_reportback_file f
+// 										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
+// 										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
+// 										WHERE f.status = \"excluded\"
+// 										AND r.rogue_post_id > $last_excluded
+// 										ORDER BY r.rogue_post_id");
+// }
+// else {
+// 	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
+// 										FROM dosomething_reportback_file f
+// 										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
+// 										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
+// 										WHERE f.status = \"excluded\"
+// 										ORDER BY r.rogue_post_id");
+// }
 
 $client = dosomething_rogue_client();
 
 foreach ($promoted_posts_in_rogue as $promoted) {
+	echo 'Checking out Rogue post ' . $promoted->rogue_post_id . PHP_EOL;
     // @TODO: bust this out into a helper
-    // @TODO: see if the post is tagged in Rogue
-    if ($last_promoted) {
+    if (isset($last_promoted)) {
 	    $northstar_id = dosomething_user_get_northstar_id($promoted->uid);
-	    $filter = [
+	    $params['filter'] = [
 			'campaign_id' => $promoted->nid,
 			'northstar_id' => $northstar_id,
 	    ];
-	    $params['filter'] = $filter;
 
-	    // @TODO: write getPosts
-	    // @TODO: bust posting tag out into a helper that takes post id and tag
+	    // Grab all posts for the given user for the given campaign from Rogue so we can check for existing tags
 	    $posts = $client->getPosts($params);
 
-	    // @TODO: if the tag already exists, continue
+
+	    // Grab the particular post we are looking for from the response
+	    $rogue_post = null;
+	    foreach ($posts['data'] as $post) {
+			if ($post['id'] == $promoted->rogue_post_id) {
+				$rogue_post = $post;
+			}
+	    }
+
+	    // If we cannot find the post in Rogue, skip it
+	    if (!$rogue_post) {
+			echo 'Could not find post ' . $promoted->rogue_post_id . ' in Rogue' . PHP_EOL;
+			continue;
+	    }
+
+	    // If the post is already tagged good-photo in Rogue, skip it
+	    if (in_array('good-photo', $rogue_post['tagged'])) {
+			echo 'Post ' . $promoted->rogue_post_id . ' already tagged good-photo in Rogue' . PHP_EOL;
+
+			// We can say that this post is done because it is already marked
+			variable_set('dosomething_rogue_last_post_promoted', $promoted->rogue_post_id);
+
+			continue;
+	    }
     }
 
-	// Format request
+	// Format request to tag post
     $data = [
       'post_id' => $promoted->rogue_post_id,
       'tag_name' => 'Good Photo',
     ];
 
     // Post tag to Rogue
-    // @TODO: write postTag
     try {
+		echo 'Trying to add tag Good Photo to post id ' . $promoted->rogue_post_id . '...' . PHP_EOL;
 		$response = $client->postTag($data);
 
-		// handle failure
-		// @TODO: how to make sure we tag everything and only send the request once? resending again on a post will cause it to be untagged - only send it if not tagged in Rogue? would need to get the post from Rogue
 		if ($response) {
-			echo 'Tagged Rogue post ' . $promoted->rogue_post_id . ' as Good Photo';
+			echo 'Tagged Rogue post ' . $promoted->rogue_post_id . ' as Good Photo' . PHP_EOL;
 
 			variable_set('dosomething_rogue_last_post_promoted', $promoted->rogue_post_id);
 		}
 		else {
-			echo '***FAILED TO TAG ROGUE POST '. $promoted->rogue_post_id . ' as Good Photo***';
+			// Handle failure
+			echo '***FAILED TO TAG ROGUE POST '. $promoted->rogue_post_id . ' as Good Photo***' . PHP_EOL;
 		}
     }
     catch (GuzzleHttp\Exception\ServerException $e) {
 		// These aren't yet caught by Gateway
-		echo '***SERVER EXCEPTION ON ROGUE POST '. $promoted->rogue_post_id . ' as Good Photo***';
+		echo '***SERVER EXCEPTION ON ROGUE POST '. $promoted->rogue_post_id . ' as Good Photo***' . PHP_EOL;
     }
     catch (DoSomething\Gateway\Exceptions\ApiException $e) {
-		echo '***API EXCEPTION ON ROGUE POST '. $promoted->rogue_post_id . ' as Good Photo***';
+		echo '***API EXCEPTION ON ROGUE POST '. $promoted->rogue_post_id . ' as Good Photo***' . PHP_EOL;
     }
-
 }
 
 // repeat for excluded
