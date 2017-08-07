332c28c7fc39704f7c037dc1694551876981a7c2
katiecrane <kcrane@dosomething.org>
2017-08-07 17:13:50 -0400

    Add draft of excluded

--- scripts/tag-as-excluded-promoted-in-rogue.php
+++ scripts/tag-as-excluded-promoted-in-rogue.php
@@ -32,26 +32,27 @@ else {
 }
 
 // Get all the excluded posts to send
-// if ($last_excluded) {
-// 	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
-// 										FROM dosomething_reportback_file f
-// 										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
-// 										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
-// 										WHERE f.status = \"excluded\"
-// 										AND r.rogue_post_id > $last_excluded
-// 										ORDER BY r.rogue_post_id");
-// }
-// else {
-// 	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
-// 										FROM dosomething_reportback_file f
-// 										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
-// 										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
-// 										WHERE f.status = \"excluded\"
-// 										ORDER BY r.rogue_post_id");
-// }
+if ($last_excluded) {
+	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
+										FROM dosomething_reportback_file f
+										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
+										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
+										WHERE f.status = \"excluded\"
+										AND r.rogue_post_id > $last_excluded
+										ORDER BY r.rogue_post_id");
+}
+else {
+	$excluded_posts_in_rogue = db_query("SELECT r.rogue_post_id, rb.uid, rb.nid
+										FROM dosomething_reportback_file f
+										JOIN dosomething_rogue_reportbacks r ON f.fid = r.fid
+										JOIN dosomething_reportback rb ON f.rbid = rb.rbid
+										WHERE f.status = \"excluded\"
+										ORDER BY r.rogue_post_id");
+}
 
 $client = dosomething_rogue_client();
 
+// Tag promoted posts
 foreach ($promoted_posts_in_rogue as $promoted) {
 	echo 'Checking out Rogue post ' . $promoted->rogue_post_id . PHP_EOL;
     // @TODO: bust this out into a helper
@@ -121,4 +122,72 @@ foreach ($promoted_posts_in_rogue as $promoted) {
     }
 }
 
-// repeat for excluded
+// Tag excluded posts
+foreach ($excluded_posts_in_rogue as $excluded) {
+	echo 'Checking out Rogue post ' . $excluded->rogue_post_id . PHP_EOL;
+    // @TODO: bust this out into a helper
+    if (isset($last_excluded)) {
+	    $northstar_id = dosomething_user_get_northstar_id($excluded->uid);
+	    $params['filter'] = [
+			'campaign_id' => $excluded->nid,
+			'northstar_id' => $northstar_id,
+	    ];
+
+	    // Grab all posts for the given user for the given campaign from Rogue so we can check for existing tags
+	    $posts = $client->getPosts($params);
+
+
+	    // Grab the particular post we are looking for from the response
+	    $rogue_post = null;
+	    foreach ($posts['data'] as $post) {
+			if ($post['id'] == $excluded->rogue_post_id) {
+				$rogue_post = $post;
+			}
+	    }
+
+	    // If we cannot find the post in Rogue, skip it
+	    if (!$rogue_post) {
+			echo 'Could not find post ' . $excluded->rogue_post_id . ' in Rogue' . PHP_EOL;
+			continue;
+	    }
+
+	    // If the post is already tagged hide-in-gallery in Rogue, skip it
+	    if (in_array('hide-in-gallery', $rogue_post['tagged'])) {
+			echo 'Post ' . $excluded->rogue_post_id . ' already tagged hide-in-gallery in Rogue' . PHP_EOL;
+
+			// We can say that this post is done because it is already marked
+			variable_set('dosomething_rogue_last_post_excluded', $excluded->rogue_post_id);
+
+			continue;
+	    }
+    }
+
+	// Format request to tag post
+    $data = [
+      'post_id' => $excluded->rogue_post_id,
+      'tag_name' => 'Hide In Gallery',
+    ];
+
+    // Post tag to Rogue
+    try {
+		echo 'Trying to add tag Hide In Gallery to post id ' . $excluded->rogue_post_id . '...' . PHP_EOL;
+		$response = $client->postTag($data);
+
+		if ($response) {
+			echo 'Tagged Rogue post ' . $excluded->rogue_post_id . ' as Hide In Gallery' . PHP_EOL;
+
+			variable_set('dosomething_rogue_last_post_excluded', $excluded->rogue_post_id);
+		}
+		else {
+			// Handle failure
+			echo '***FAILED TO TAG ROGUE POST '. $excluded->rogue_post_id . ' as Hide In Gallery***' . PHP_EOL;
+		}
+    }
+    catch (GuzzleHttp\Exception\ServerException $e) {
+		// These aren't yet caught by Gateway
+		echo '***SERVER EXCEPTION ON ROGUE POST '. $excluded->rogue_post_id . ' as Hide In Gallery***' . PHP_EOL;
+    }
+    catch (DoSomething\Gateway\Exceptions\ApiException $e) {
+		echo '***API EXCEPTION ON ROGUE POST '. $excluded->rogue_post_id . ' as Hide In Gallery***' . PHP_EOL;
+    }
+}
