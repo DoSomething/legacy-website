021798ac0911aca88c43af5975a6357597c1da9f
Mike Crittenden <mikecrittenden@gmail.com>
2017-09-15 16:15:19 -0400

    Disable host checking for the staging site

--- env/postprovision.sh
+++ env/postprovision.sh
@@ -4,4 +4,5 @@
 cp /vagrant/config/* /var/www/dev.dosomething.org/html/sites/default/
 
 # Grab a copy of the Staging DB
+ssh -o "StrictHostKeyChecking no" staging.beta.dosomething.org
 drush sql-sync @ds.staging @ds.local --yes
