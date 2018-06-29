db25ad5dc0b20b0f200aebdf02766f3bdffaa47f
Diego Lorenzo <diego@diegolorenzo.com>
2018-06-29 14:12:04 -0400

    Adding feature flag for Google Tag Manager script inclusion.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
@@ -51,7 +51,17 @@
       fbq('init', '809882989202123');
       fbq('track', 'PageView');
     </script>
-    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=809882989202123&ev=PageView&noscript=1" /></noscript>
+  <?php endif; ?>
+
+  <?php if ($variables['use_google_tag_manager']): ?>
+    <!-- Google Tag Manager -->
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NNQ2GQQ');
+    </script>
   <?php endif; ?>
 </head>
 
