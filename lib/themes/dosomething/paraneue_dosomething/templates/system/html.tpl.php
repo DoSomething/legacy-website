8f123cc9cd44583d2a99e686f032a110ae4ea752
Diego Lorenzo <diego@diegolorenzo.com>
2018-06-29 14:18:43 -0400

    Removing old enable ad tracking stuff.

--- lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
+++ lib/themes/dosomething/paraneue_dosomething/templates/system/html.tpl.php
@@ -73,16 +73,6 @@
 
   <?php print $scripts; ?>
 
-  <?php if ($variables['enable_ad_tracking']): ?>
-    <!-- Twitter single-event website tag code -->
-    <script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
-    <script type="text/javascript">twttr.conversion.trackPid('nvo4z', { tw_sale_amount: 0, tw_order_quantity: 0 });</script>
-    <noscript>
-    <img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=nvo4z&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />
-    <img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=nvo4z&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />
-    </noscript>
-  <?php endif; ?>
-
   <script src="https://my.hellobar.com/f70465b5f088ff5df39e838a358d27109b365641.js" type="text/javascript" charset="utf-8" async="async"></script>
 </body>
 
