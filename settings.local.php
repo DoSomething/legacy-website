d6ba56985d17c9f2f6cc9ba3ae37b3b831a027c3
Mike Crittenden <mikecrittenden@gmail.com>
2018-07-13 10:39:32 -0400

    Add missing settings.local.php

---
+++ settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+// Point database to local service.
+$databases['default']['default'] = array(
+  'database' => 'default',
+  'username' => 'user',
+  'password' => 'user',
+  'host' => 'db',
+  'port' => '',
+  'driver' => 'mysql',
+  'prefix' => '',
+);
